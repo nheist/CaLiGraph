abd538504aff236e14301aa05ef640f1bae96d58
nheist <nico@informatik.uni-mannheim.de>
2018-09-18 15:39:03 +0200

    FIX: correct creation of graph

--- caligraph/category/graph.py
+++ caligraph/category/graph.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from . import store as cat_store
+import util
 
 
 class CategoryGraph:
@@ -17,6 +18,7 @@ class CategoryGraph:
         return CategoryGraph(nx.bfs_tree(self.graph, self.root_node), self.root_node)
 
     @classmethod
-    def create_from_dbpedia(cls):
+    def create_from_dbpedia(cls, root_node=None):
         edges = [(node, child) for node in cat_store.get_all_cats() for child in cat_store.get_children(node)]
-        return nx.DiGraph(incoming_graph_data=edges)
+        root_node = root_node if root_node else util.get_config('caligraph.category.root_node')
+        return CategoryGraph(nx.DiGraph(incoming_graph_data=edges), root_node)
