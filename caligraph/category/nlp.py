17243f9450f69bea595adc4d808a95b1bad114c6
nheist <nico@informatik.uni-mannheim.de>
2018-09-20 14:41:11 +0200

    TASK: Finalize conceptual category recognition

--- caligraph/category/nlp.py
+++ caligraph/category/nlp.py
@@ -5,9 +5,12 @@ import util
 
 def is_conceptual(category: str) -> bool:
     label = cat_store.get_label(category)
-    parsed_label = nlp_util.parse(label)
-    util.get_logger().debug('{} ---> {}'.format(label, list(parsed_label.noun_chunks)))
-    return False
+    label_chunks = list(nlp_util.parse(label).noun_chunks)
+
+    if len(label_chunks) == 0:
+        return False
+
+    return label_chunks[0][-1].tag_ == 'NNS'
 
 
 def singularize(category: str) -> str:
