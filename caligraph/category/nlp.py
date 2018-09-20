a554faa0305b22b0287c02115a0ae5e52a7648dc
nheist <nico@informatik.uni-mannheim.de>
2018-09-20 10:24:19 +0200

    FIX: More debugging for nlp

--- caligraph/category/nlp.py
+++ caligraph/category/nlp.py
@@ -6,7 +6,7 @@ import util
 def is_conceptual(category: str) -> bool:
     label = cat_store.get_label(category)
     parsed_label = nlp_util.parse(label)
-    util.get_logger().debug('category.nlp: LABEL: {} --- CKUNKS: {}'.format(label, parsed_label.noun_chunks))
+    util.get_logger().debug('{} ---> {}'.format(label, list(parsed_label.noun_chunks)))
     return False
 
 
