90f621d735744976c19217e4b1586ac238d27d29
nheist <nico@informatik.uni-mannheim.de>
2018-09-21 18:21:12 +0200

    TASK: Write cache only if necessary

--- caligraph/util/nlp.py
+++ caligraph/util/nlp.py
@@ -11,8 +11,9 @@ parser = spacy.load('en_core_web_lg')
 
 def parse(text: str, skip_cache=False) -> Doc:
     if '__NLP_CACHE__' not in globals():
-        global __NLP_CACHE__
+        global __NLP_CACHE__, __NLP_CACHE_CHANGED__
         __NLP_CACHE__ = util.load_or_create_cache(SPACY_CACHE_ID, lambda: dict())
+        __NLP_CACHE_CHANGED__ = False
 
     text_hash = hashlib.md5(text.encode('utf-8')).digest()
     if text_hash in __NLP_CACHE__:
@@ -21,8 +22,12 @@ def parse(text: str, skip_cache=False) -> Doc:
     parsed_text = parser(text)
     if not skip_cache:
         __NLP_CACHE__[text_hash] = parsed_text
+        __NLP_CACHE_CHANGED__ = True
     return parsed_text
 
 
 def persist_cache():
-    util.update_cache(SPACY_CACHE_ID, __NLP_CACHE__)
+    global __NLP_CACHE_CHANGED__
+    if __NLP_CACHE_CHANGED__:
+        util.update_cache(SPACY_CACHE_ID, __NLP_CACHE__)
+        __NLP_CACHE_CHANGED__ = False
