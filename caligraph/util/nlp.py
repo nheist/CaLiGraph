864eb9d5178b5384c8a95ee48c01844960876311
nheist <nico@informatik.uni-mannheim.de>
2018-09-21 16:53:27 +0200

    FIX: Move cache initialization to first usage

--- caligraph/util/nlp.py
+++ caligraph/util/nlp.py
@@ -7,19 +7,22 @@ SPACY_CACHE_ID = 'spacy_docs'
 
 # initialization
 parser = spacy.load('en_core_web_lg')
-cache = util.load_or_create_cache(SPACY_CACHE_ID, lambda: dict())
 
 
 def parse(text: str, skip_cache=False) -> Doc:
+    if '__NLP_CACHE__' not in globals():
+        global __NLP_CACHE__
+        __NLP_CACHE__ = util.load_or_create_cache(SPACY_CACHE_ID, lambda: dict())
+
     text_hash = hashlib.md5(text.encode('utf-8')).digest()
-    if text_hash in cache:
-        return cache[text_hash]
+    if text_hash in __NLP_CACHE__:
+        return __NLP_CACHE__[text_hash]
 
     parsed_text = parser(text)
     if not skip_cache:
-        cache[text_hash] = parsed_text
+        __NLP_CACHE__[text_hash] = parsed_text
     return parsed_text
 
 
 def persist_cache():
-    util.update_cache(SPACY_CACHE_ID, cache)
+    util.update_cache(SPACY_CACHE_ID, __NLP_CACHE__)
