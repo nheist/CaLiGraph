345c720b3d10c699010a498d3057a2a41c5311c3
nheist <nico@informatik.uni-mannheim.de>
2018-09-22 09:17:24 +0200

    REFACTOR: Improve interface of graph

--- caligraph/category/graph.py
+++ caligraph/category/graph.py
@@ -11,12 +11,15 @@ class CategoryGraph:
         self.root_node = root_node
 
     @property
-    def node_count(self) -> int:
-        return self.graph.number_of_nodes()
-
-    @property
-    def edge_count(self) -> int:
-        return self.graph.number_of_edges()
+    def statistics(self) -> str:
+        return '\n'.join([
+            '{:^40}'.format('CATEGORY GRAPH'),
+            '=' * 40,
+            '{:>18} | {:>19}'.format('nodes', self.graph.number_of_nodes()),
+            '{:>18} | {:>19}'.format('edges', self.graph.number_of_edges()),
+            '{:>18} | {:>19}'.format('in-degree', self.graph.in_degree),
+            '{:>18} | {:>19}'.format('out-degree', self.graph.out_degree)
+        ])
 
     def predecessors(self, node: str) -> set:
         return set(self.graph.predecessors(node))
@@ -25,28 +28,32 @@ class CategoryGraph:
         return set(self.graph.successors(node))
 
     def remove_unconnected(self):
-        valid_nodes = list(nx.bfs_tree(self.graph, self.root_node))
-        return CategoryGraph(self.graph.subgraph(valid_nodes), self.root_node)
+        valid_nodes = set(nx.bfs_tree(self.graph, self.root_node))
+        self._remove_all_nodes_except(valid_nodes)
+        return self
 
     def append_unconnected(self):
         unconnected_root_nodes = {node for node in self.graph.nodes if len(self.predecessors(node)) == 0 and node != self.root_node}
-        new_edges = [(self.root_node, node) for node in unconnected_root_nodes]
-        return CategoryGraph(nx.DiGraph(incoming_graph_data=(list(self.graph.edges) + new_edges)), self.root_node)
+        self.graph.add_edges_from([(self.root_node, node) for node in unconnected_root_nodes])
+        return self
 
-    def get_conceptual_graph(self):
+    def make_conceptual(self):
         categories = set(self.graph.nodes)
         # filtering maintenance categories
         categories = categories.difference(cat_store.get_maintenance_cats())
         # filtering administrative categories
-        categories = [cat for cat in categories if not cat.endswith(('templates', 'navigational boxes'))]
+        categories = {cat for cat in categories if not cat.endswith(('templates', 'navigational boxes'))}
         # filtering non-conceptual categories
-        categories = [cat for cat in categories if cat_nlp.is_conceptual(cat) or cat == self.root_node]
-
+        categories = {cat for cat in categories if cat_nlp.is_conceptual(cat)}
         # persisting spacy cache so that parsed categories are cached
         nlp_util.persist_cache()
 
-        # todo: connect unconnected nodes
-        return CategoryGraph(self.graph.subgraph(categories), self.root_node)
+        self._remove_all_nodes_except(categories | {self.root_node})
+        return self
+
+    def _remove_all_nodes_except(self, valid_nodes: set):
+        invalid_nodes = set(self.graph.nodes).difference(valid_nodes)
+        self.graph.remove_nodes_from(invalid_nodes)
 
     @classmethod
     def create_from_dbpedia(cls, root_node=None):
