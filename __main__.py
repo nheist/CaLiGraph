266a81ff281789d5560f5b5374ecb51829cd4d50
nheist <nico@informatik.uni-mannheim.de>
2018-10-08 21:11:09 +0200

    TASK: Test new impurity measure

--- __main__.py
+++ __main__.py
@@ -1,6 +1,9 @@
 import relation_test
+from caligraph.category.base import get_cycle_free_category_graph
 
 
 if __name__ == '__main__':
-    relation_test.evaluate_category_relations()
-
+    # relation_test.evaluate_category_relations()
+    G = get_cycle_free_category_graph()
+    G.assign_dbp_types()
+    print(G.statistics)
