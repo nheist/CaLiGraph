6d4c48f40a467872533d04334eeae9d2a00991b9
nheist <nico@informatik.uni-mannheim.de>
2018-10-09 09:26:49 +0200

    TASK: run wikidata extraction

--- __main__.py
+++ __main__.py
@@ -1,9 +1,13 @@
 import relation_test
 from caligraph.category.base import get_cycle_free_category_graph
+import caligraph.wikidata.store as wikidata_store
 
 
 if __name__ == '__main__':
     # relation_test.evaluate_category_relations()
-    G = get_cycle_free_category_graph()
-    G.assign_dbp_types()
-    print(G.statistics)
+
+    # G = get_cycle_free_category_graph()
+    # G.assign_dbp_types()
+    # print(G.statistics)
+
+    print('RESULT: {}'.format(wikidata_store.resource_has_type('http://dbpedia.org/resource/Cristiano_Ronaldo', 'http://dbpedia.org/ontology/Person')))
