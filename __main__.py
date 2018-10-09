610033de1a803cc3fe9107734f8eadd0487ce118
nheist <nico@informatik.uni-mannheim.de>
2018-10-09 12:02:18 +0200

    TASK: Run dbp-typed extraction

--- __main__.py
+++ __main__.py
@@ -1,12 +1,15 @@
+import util
 import relation_test
 import caligraph.category.base as cat_base
 import caligraph.wikidata.store as wikidata_store
-import util
 
 if __name__ == '__main__':
+    util.get_logger().info('Starting CaLiGraph extraction..')
     # relation_test.evaluate_category_relations()
 
     G = cat_base.get_dbp_typed_category_graph()
     util.get_logger().info(G.statistics)
 
     # print('RESULT: {}'.format(wikidata_store.resource_has_type('http://dbpedia.org/resource/Cristiano_Ronaldo', 'http://dbpedia.org/ontology/Person')))
+
+    util.get_logger().info('Finished CaLiGraph extraction.')
