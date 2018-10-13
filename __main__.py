cbf4284b70c854342897fd4c08e2d823c6d485a8
nheist <nico@informatik.uni-mannheim.de>
2018-10-13 11:33:55 +0200

    TASK: Run another init of wikidata resource mappings

--- __main__.py
+++ __main__.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
     util.get_logger().info('Starting CaLiGraph extraction..')
     # relation_test.evaluate_category_relations()
 
-    G = cat_base.get_dbp_typed_category_graph()
-    util.get_logger().info(G.statistics)
+    # G = cat_base.get_dbp_typed_category_graph()
+    # util.get_logger().info(G.statistics)
 
-    # print('RESULT: {}'.format(wikidata_store.resource_has_type('http://dbpedia.org/resource/Cristiano_Ronaldo', 'http://dbpedia.org/ontology/Person')))
+    print('RESULT: {}'.format(wikidata_store.resource_has_type('http://dbpedia.org/resource/Cristiano_Ronaldo', 'http://dbpedia.org/ontology/Person')))
 
     util.get_logger().info('Finished CaLiGraph extraction.')
