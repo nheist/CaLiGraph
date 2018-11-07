403c9e744516e3068dc03488c65fe49322772966
nheist <nico@informatik.uni-mannheim.de>
2018-11-07 15:58:57 +0100

    FIX: Correct computation of functional predicates

--- caligraph/dbpedia/store.py
+++ caligraph/dbpedia/store.py
@@ -149,12 +149,12 @@ def is_functional(dbp_predicate: str) -> bool:
 
 
 def _create_functional_predicate_dict():
-    predicate_functionality = {pred: False for pred in get_all_predicates()}
+    predicate_functionality = {pred: True for pred in get_all_predicates()}
     resource_property_mapping = get_resource_property_mapping()
     for r in resource_property_mapping:
         for pred in resource_property_mapping[r]:
             if len(resource_property_mapping[r][pred]) > 1:
-                predicate_functionality[pred] = True
+                predicate_functionality[pred] = False
 
     return predicate_functionality
 
