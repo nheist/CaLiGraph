b7f55de3cd3f186320ccdc779470e5a347eadb35
nheist <nico@informatik.uni-mannheim.de>
2018-11-20 16:34:07 +0100

    TASK: Run bayesian + RR

--- relation_test.py
+++ relation_test.py
@@ -12,8 +12,8 @@ import functools
 import operator
 
 COMPUTE_BASELINE = False
-USE_HEURISTIC_CONSTRAINTS = True  # HC
-USE_RESOLVED_REDIRECTS = False  # RR
+USE_HEURISTIC_CONSTRAINTS = False  # HC
+USE_RESOLVED_REDIRECTS = True  # RR
 
 CategoryProperty = namedtuple('CategoryProperty', 'cat pred obj prob count inv')
 
