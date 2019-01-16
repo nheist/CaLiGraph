4f214cc8cca685eff6755de7e0d216a38a592def
nheist <nico@informatik.uni-mannheim.de>
2019-01-16 10:04:41 +0100

    TASK: Adapt main to run dbp-type evaluation

--- __main__.py
+++ __main__.py
@@ -2,12 +2,19 @@ import traceback
 import util
 import mailer
 import impl.category.axioms as cat_axioms
+from impl.category.evaluation import test_metrics
 
 if __name__ == '__main__':
     try:
         util.get_logger().info('Starting CaLiGraph extraction..')
 
-        cat_axioms.extract_axioms_and_relation_assertions()
+        # TASK: extraction of category axioms
+        # cat_axioms.extract_axioms_and_relation_assertions()
+
+        # TASK: evaluation of assigned dbp-types to catgraph
+        graph = util.load_cache('catgraph_cyclefree')
+        graph._assign_resource_type_counts()
+        test_metrics(graph)
 
         mailer.send_success()
         util.get_logger().info('Finished CaLiGraph extraction.')
