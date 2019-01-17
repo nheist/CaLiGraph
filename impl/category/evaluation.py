88c1be0b284b53bb9f0c24d0f306de047530e1c0
nheist <nico@informatik.uni-mannheim.de>
2019-01-17 12:47:13 +0100

    TASK: use only resource/child ratio

--- impl/category/evaluation.py
+++ impl/category/evaluation.py
@@ -12,15 +12,17 @@ import util
 def test_metrics(graph: CategoryGraph):
     util.get_logger().debug('Evaluation of parameters for dbp-type extraction..')
 
-    columns = ['apply_impure_type_filtering', 'exclude_untyped_resources', 'prefer_resource_types', 'apply_type_depth_smoothing', 'resource_type_ratio', 'child_type_ratio', 'precision', 'recall', 'F1']
-    data = list(itertools.product([True, False], [True, False], [True, False], [True, False], np.linspace(.1, 1, 10), np.linspace(.1, 1, 10), [0.0], [0.0], [0.0]))
+    #columns = ['apply_impure_type_filtering', 'exclude_untyped_resources', 'prefer_resource_types', 'apply_type_depth_smoothing', 'resource_type_ratio', 'child_type_ratio', 'precision', 'recall', 'F1']
+    columns = ['exclude_untyped_resources', 'prefer_resource_types', 'resource_type_ratio', 'child_type_ratio', 'precision', 'recall', 'F1']
+    #data = list(itertools.product([True, False], [True, False], [True, False], [True, False], np.linspace(.4, 1, 7), np.linspace(.4, 1, 7), [0.0], [0.0], [0.0]))
+    data = list(itertools.product([True, False], [True, False], np.linspace(.4, 1, 7), np.linspace(.4, 1, 7), [0.0], [0.0], [0.0]))
     df = pd.DataFrame(data=data, columns=columns)
 
     for idx, row in enumerate(df.itertuples()):
-        util.set_config('category.dbp_types.apply_impure_type_filtering', row.apply_impure_type_filtering)
+        #util.set_config('category.dbp_types.apply_impure_type_filtering', row.apply_impure_type_filtering)
         util.set_config('category.dbp_types.exclude_untyped_resources', row.exclude_untyped_resources)
         util.set_config('category.dbp_types.prefer_resource_types', row.prefer_resource_types)
-        util.set_config('category.dbp_types.apply_type_depth_smoothing', row.apply_type_depth_smoothing)
+        #util.set_config('category.dbp_types.apply_type_depth_smoothing', row.apply_type_depth_smoothing)
         util.set_config('category.dbp_types.resource_type_ratio', row.resource_type_ratio)
         util.set_config('category.dbp_types.child_type_ratio', row.child_type_ratio)
 
@@ -31,7 +33,7 @@ def test_metrics(graph: CategoryGraph):
         df.at[row.Index, 'precision'] = precision
         df.at[row.Index, 'recall'] = recall
         df.at[row.Index, 'F1'] = f1
-        util.get_logger().debug(f'RUN {idx}/{len(df.index)}: P={precision*100:.2f} R={recall*100:.2f} F1={f1*100:.2f}')
+        util.get_logger().debug(f'RUN {idx}/{len(df.index)} with [{row.exclude_untyped_resources}|{row.prefer_resource_types}|{row.resource_type_ratio:.1f}|{row.child_type_ratio:.1f}]: P={precision*100:.2f} R={recall*100:.2f} F1={f1*100:.2f}')
 
     df.to_csv('catgraph-dbptype_evaluation.csv')
     util.get_logger().debug('Finished evaluation of parameters for dbp-type extraction.')
