3c6e7f4006da2434386c1697e977d3958b7835ec
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-04-15 15:23:45 +0200

    REFACTOR: move generation of category statistics to catstore

--- __main__.py
+++ __main__.py
@@ -2,31 +2,12 @@ import traceback
 import util
 import mailer
 import impl.list.base as list_base
-import impl.category.axioms as cat_axioms
-# from impl.category.evaluation import test_metrics
 
 if __name__ == '__main__':
     try:
         util.get_logger().info('Starting CaLiGraph extraction..')
 
-        # TASK: extraction of category axioms
-        #cat_axioms.extract_axioms_and_relation_assertions()
-        # TASK: create cataxioms data
-        def get_data_X_y(version):
-            candidates = util.load_or_create_cache('cataxioms_candidates', cat_axioms._compute_candidate_axioms, version=version)
-            X, y = cat_axioms._create_goldstandard(candidates)
-            return candidates, X, y
-        get_data_X_y('2b')
-
-        # TASK: evaluation of assigned dbp-types to catgraph
-        # graph = util.load_cache('catgraph_cyclefree')
-        # graph._assign_resource_type_counts()
-        # test_metrics(graph)
-
-        # TASK: listpage markup extraction
-        #list_base.get_parsed_listpages()
-
-        mailer.send_success('extracted cataxioms markup.')
+        mailer.send_success('WHAT WHAT WHAT')
         util.get_logger().info('Finished CaLiGraph extraction.')
     except Exception as e:
         error_msg = traceback.format_exc()
--- impl/category/store.py
+++ impl/category/store.py
@@ -1,6 +1,8 @@
 from . import util as cat_util
 import impl.util.rdf as rdf_util
 import util
+from collections import defaultdict
+import impl.dbpedia.store as dbp_store
 
 
 def get_all_cats() -> set:
@@ -93,3 +95,61 @@ def get_maintenance_cats() -> set:
         __MAINTENANCE_CATS__ = set(rdf_util.create_single_val_dict_from_rdf([util.get_data_file('files.dbpedia.maintenance_categories')], rdf_util.PREDICATE_TYPE))
 
     return __MAINTENANCE_CATS__
+
+
+def get_statistics(category: str) -> dict:
+    global __CATEGORY_STATISTICS__
+    if '__CATEGORY_STATISTICS__' not in globals():
+        __CATEGORY_STATISTICS__ = util.load_or_create_cache('dbpedia_category_statistics', _compute_category_statistics)
+
+    return __CATEGORY_STATISTICS__[category]
+
+
+def _compute_category_statistics() -> dict:
+    category_statistics = {}
+    for cat in get_all_cats():
+        type_counts = defaultdict(int)
+        property_counts = defaultdict(int)
+        property_counts_inv = defaultdict(int)
+        predicate_counts = defaultdict(int)
+        predicate_counts_inv = defaultdict(int)
+
+        resources = get_resources(cat)
+        for res in resources:
+            resource_statistics = _compute_resource_statistics(res)
+            for t in resource_statistics['types']:
+                type_counts[t] += 1
+            for prop in resource_statistics['properties']:
+                property_counts[prop] += 1
+            for prop in resource_statistics['properties_inv']:
+                property_counts_inv[prop] += 1
+            for pred in resource_statistics['predicates']:
+                predicate_counts[pred] += 1
+            for pred in resource_statistics['predicates_inv']:
+                predicate_counts_inv[pred] += 1
+        category_statistics[cat] = {
+            'type_counts': type_counts,
+            'type_frequencies': defaultdict(float, {t: t_count / len(resources) for t, t_count in type_counts.items()}),
+            'property_counts': property_counts,
+            'property_counts_inv': property_counts_inv,
+            'property_frequencies': defaultdict(float, {prop: p_count / len(resources) for prop, p_count in property_counts.items()}),
+            'property_frequencies_inv': defaultdict(float, {prop: p_count / len(resources) for prop, p_count in property_counts_inv.items()}),
+            'predicate_counts': predicate_counts,
+            'predicate_counts_inv': predicate_counts_inv
+        }
+    return category_statistics
+
+
+def _compute_resource_statistics(dbp_resource: str) -> dict:
+    global __RESOURCE_STATISTICS__
+    if '__RESOURCE_STATISTICS__' not in globals():
+        __RESOURCE_STATISTICS__ = {}
+    if dbp_resource not in __RESOURCE_STATISTICS__:
+        __RESOURCE_STATISTICS__[dbp_resource] = {
+            'types': dbp_store.get_transitive_types(dbp_resource),
+            'properties': {(pred, val) for pred, values in dbp_store.get_properties(dbp_resource).items() for val in values},
+            'properties_inv': {(pred, val) for pred, values in dbp_store.get_inverse_properties(dbp_resource).items() for val in values},
+            'predicates': {pred for pred in dbp_store.get_properties(dbp_resource)},
+            'predicates_inv': {pred for pred in dbp_store.get_inverse_properties(dbp_resource)},
+        }
+    return __RESOURCE_STATISTICS__[dbp_resource]
