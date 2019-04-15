1d84f3f241c8802e4d37d0a2096d3523fd88243e
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-04-15 16:55:44 +0200

    TASK: implement final category conceptualisation

--- impl/category/nlp.py
+++ impl/category/nlp.py
@@ -2,25 +2,50 @@ import impl.util.nlp as nlp_util
 import impl.category.store as cat_store
 import util
 import inflection
+import spacy
 
-CONCEPTUAL_CATEGORIES_CACHE_ID = 'categories_conceptual'
 SINGULARIZED_CATEGORIES_CACHE_ID = 'categories_singularized'
 
 
 def is_conceptual(category: str) -> bool:
     global __CONCEPTUAL_CATEGORIES__
     if '__CONCEPTUAL_CATEGORIES__' not in globals():
-        __CONCEPTUAL_CATEGORIES__ = util.load_or_create_cache(CONCEPTUAL_CATEGORIES_CACHE_ID, lambda: dict())
+        __CONCEPTUAL_CATEGORIES__ = util.load_or_create_cache('categories_conceptual', _compute_conceptual_categories)
 
-    if category not in __CONCEPTUAL_CATEGORIES__:
-        label = cat_store.get_label(category)
-        split_label = label.split(' ')
-        if len(split_label) > 1 and not (label[1].isupper() or split_label[1][0].isupper()):
-            label = label[0].lower() + label[1:]
-        label_chunks = list(nlp_util.parse(label).noun_chunks)
-        __CONCEPTUAL_CATEGORIES__[category] = label_chunks and label.startswith(label_chunks[0].text) and label_chunks[0][-1].tag_ == 'NNS'
+    return category in __CONCEPTUAL_CATEGORIES__
 
-    return __CONCEPTUAL_CATEGORIES__[category]
+
+def _compute_conceptual_categories() -> set:
+    # TODO: implement check with category sets to find remaining categories (eg. the 5k albums that we are missing out)
+    conceptual_caegories = set()
+    for cat in cat_store.get_all_cats():
+        lexhead = get_lexical_head(cat)
+        if lexhead:
+            main_token = lexhead if type(lexhead) == spacy.tokens.Token else lexhead[-1]
+            if main_token.tag_ == 'NNS':
+                conceptual_caegories.add(cat)
+    return conceptual_caegories
+
+
+def get_lexical_head(category: str):
+    label = cat_store.get_label(category)
+    split_label = label.split(' ')
+    if len(split_label) > 1 and not (label[1].isupper() or split_label[1][0].isupper()):
+        label = label[0].lower() + label[1:]
+    doc = nlp_util.parse(label)
+    for chunk in doc.noun_chunks:
+        elem = chunk.root
+        if (elem.text.istitle() and elem != doc[0]) or elem.text == '–':
+            continue
+        if len(doc) > elem.i + 1 and doc[elem.i+1].text == ')':
+            continue
+        if len(doc) > elem.i + 2 and doc[elem.i+1].text == 'and':
+            if doc[elem.i+2].pos_ == 'NOUN':
+                elem = doc[elem.i:elem.i+3]
+            elif len(doc) > elem.i + 3 and doc[elem.i+2].pos_ == 'ADJ' and doc[elem.i+3].pos_ == 'NOUN':
+                elem = doc[elem.i:elem.i+4]
+        return elem
+    return None
 
 
 def singularize(category: str) -> str:
@@ -45,7 +70,5 @@ def singularize(category: str) -> str:
 
 def persist_cache():
     nlp_util.persist_cache()
-    if '__CONCEPTUAL_CATEGORIES__' in globals():
-        util.update_cache(CONCEPTUAL_CATEGORIES_CACHE_ID, __CONCEPTUAL_CATEGORIES__)
     if '__SINGULARIZED_CATEGORIES__' in globals():
         util.update_cache(SINGULARIZED_CATEGORIES_CACHE_ID, __SINGULARIZED_CATEGORIES__)
