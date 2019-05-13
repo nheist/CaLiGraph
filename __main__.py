b9e59aef72621383229786f4ed35fd7c8cb8ffb4
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-05-13 18:36:54 +0200

    TASK: run list parsing in main

--- __main__.py
+++ __main__.py
@@ -2,14 +2,15 @@ import traceback
 import util
 import mailer
 from impl.category import cat2ax
+import impl.list.base as list_base
 
 if __name__ == '__main__':
     try:
         util.get_logger().info('Starting CaLiGraph extraction..')
 
-        cat2ax.run_extraction()
+        list_base.get_parsed_listpages()
 
-        mailer.send_success('FINISHED Cat2Ax')
+        mailer.send_success('FINISHED list parsing')
         util.get_logger().info('Finished CaLiGraph extraction.')
     except Exception as e:
         error_msg = traceback.format_exc()
