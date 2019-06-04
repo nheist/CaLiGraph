1b2ba6720d4526467de0bd049ed53c194f2c7bc6
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-06-04 16:05:08 +0200

    TASK: Run axiom extraction again

--- __main__.py
+++ __main__.py
@@ -2,13 +2,16 @@ import traceback
 import util
 import mailer
 #import impl.list.base as list_base
-import impl.dbpedia.heuristics as dbp_heur
+#import impl.dbpedia.heuristics as dbp_heur
+import impl.category.cat2ax as cat_axioms
+import impl.util.nlp as nlp_util
 
 if __name__ == '__main__':
     try:
         util.get_logger().info('Starting CaLiGraph extraction..')
 
-        util.get_logger().info(dbp_heur.get_disjoint_types('http://dbpedia.org/ontology/School'))
+        util.get_logger().info(cat_axioms.get_axioms('http://dbpedia.org/resource/Category:Writers'))
+        nlp_util.persist_cache()
 
         mailer.send_success('FINISHED heuristics extraction')
         util.get_logger().info('Finished CaLiGraph extraction.')
