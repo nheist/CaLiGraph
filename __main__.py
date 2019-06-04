c38a5f59d1a580950732b21f6e8296d8e2106816
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-06-04 22:22:44 +0200

    TASK: Run child-list extraction

--- __main__.py
+++ __main__.py
@@ -1,19 +1,19 @@
 import traceback
 import util
 import mailer
-#import impl.list.base as list_base
+import impl.list.store as list_store
 #import impl.dbpedia.heuristics as dbp_heur
-import impl.category.cat2ax as cat_axioms
+#import impl.category.cat2ax as cat_axioms
 import impl.util.nlp as nlp_util
 
 if __name__ == '__main__':
     try:
         util.get_logger().info('Starting CaLiGraph extraction..')
 
-        util.get_logger().info(cat_axioms.get_axioms('http://dbpedia.org/resource/Category:Writers'))
+        util.get_logger().info(list_store.get_child_listpages('http://dbpedia.org/resource/Category:Writers'))
         nlp_util.persist_cache()
 
-        mailer.send_success('FINISHED heuristics extraction')
+        mailer.send_success('FINISHED childlist extraction')
         util.get_logger().info('Finished CaLiGraph extraction.')
     except Exception as e:
         error_msg = traceback.format_exc()
