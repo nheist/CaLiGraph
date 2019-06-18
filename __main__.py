76d9a41361191de018b197b0d66b66533b3acec0
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-06-18 16:40:26 +0200

    TASK: Run list parsing v2

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import mailer
 import impl.list.store as list_store
 import impl.list.hierarchy as list_hierarchy
 import impl.list.base as list_base
+import impl.list.parser as list_parser
 #import impl.dbpedia.heuristics as dbp_heur
 #import impl.category.cat2ax as cat_axioms
 import impl.util.nlp as nlp_util
@@ -19,9 +20,11 @@ if __name__ == '__main__':
 
         #edges_found = len(cat_wikitax.get_valid_edges())
 
-        list_hierarchy.get_equivalent_listpage('')
+        #list_hierarchy.get_equivalent_listpage('')
 
-        mailer.send_success(f'FINISHED equivalent listpage extraction')
+        list_parser.get_parsed_listpages()
+
+        mailer.send_success(f'FINISHED listparse v2')
         util.get_logger().info('Finished CaLiGraph extraction.')
     except Exception as e:
         error_msg = traceback.format_exc()
