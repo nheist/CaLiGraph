24b66eb11c3607b7e40f794a52d57ad409760515
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-09-11 14:46:40 +0200

    TASK: Computation of caligraph v6

--- __main__.py
+++ __main__.py
@@ -29,13 +29,14 @@ def setup():
 
 if __name__ == '__main__':
     try:
-        util.get_logger().info('Starting listpage parsing..')
+        util.get_logger().info('Starting extraction of caligraph v6..')
 
-        list_base.get_table_listpage_entity_features()
-        nlp_util.persist_cache()
+        #list_base.get_table_listpage_entity_features()
+        #nlp_util.persist_cache()
 
-        #caligraph = cali_base.get_base_graph()
-        #util.get_logger().info(caligraph.statistics)
+        setup()
+        caligraph = cali_base.get_base_graph()
+        util.get_logger().info(caligraph.statistics)
 
         #cat_graph = cat_base.get_merged_graph()
         #util.get_logger().info('catgraph done.')
@@ -53,8 +54,8 @@ if __name__ == '__main__':
         #nlp_util.persist_cache()
         #util.get_logger().info('cache persist done.')
 
-        mailer.send_success(f'FINISHED listpage parsing')
-        util.get_logger().info('Finished listpage parsing.')
+        mailer.send_success(f'FINISHED extraction of caligraph v6')
+        util.get_logger().info('Finished extraction of caligraph v6.')
     except Exception as e:
         error_msg = traceback.format_exc()
         mailer.send_error(error_msg)
