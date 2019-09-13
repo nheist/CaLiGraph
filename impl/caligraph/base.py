236413df0afd8bb90941cef1a6bd950006862b60
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-09-13 14:32:36 +0200

    TASK: Make sure to get rid of cycles in caligraph

--- impl/caligraph/base.py
+++ impl/caligraph/base.py
@@ -5,5 +5,6 @@ import util
 def get_base_graph() -> CaLiGraph:
     global __BASE_GRAPH__
     if '__BASE_GRAPH__' not in globals():
-        __BASE_GRAPH__ = util.load_or_create_cache('caligraph_base', CaLiGraph.build_graph)
+        initializer = lambda: CaLiGraph.build_graph().resolve_cycles()
+        __BASE_GRAPH__ = util.load_or_create_cache('caligraph_base', initializer)
     return __BASE_GRAPH__
