1335809d6e968a1f94eaf1b9c38d0c6caee5189b
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-09-18 08:57:46 +0200

    FIX: Remove caligraph dependency in ontology mapper

--- impl/caligraph/ontology_mapper.py
+++ impl/caligraph/ontology_mapper.py
@@ -1,4 +1,3 @@
-from impl.caligraph.graph import CaLiGraph
 import impl.util.nlp as nlp_util
 import impl.dbpedia.store as dbp_store
 import impl.dbpedia.util as dbp_util
@@ -7,14 +6,14 @@ from collections import defaultdict
 import util
 
 
-def find_mappings(graph: CaLiGraph, use_listpage_resources: bool) -> dict:
+def find_mappings(graph, use_listpage_resources: bool) -> dict:
     mappings = {}
     for node in graph.children(graph.root_node):
         mappings.update(_find_mapping(graph, use_listpage_resources, node))
     return mappings
 
 
-def _find_mapping(graph: CaLiGraph, use_listpage_resources: bool, node: str) -> dict:
+def _find_mapping(graph, use_listpage_resources: bool, node: str) -> dict:
     node_types = _find_dbpedia_parents(graph, use_listpage_resources, node)
     if node_types:
         return {node: node_types}
@@ -25,7 +24,7 @@ def _find_mapping(graph: CaLiGraph, use_listpage_resources: bool, node: str) -> 
         return mapping
 
 
-def _find_dbpedia_parents(graph: CaLiGraph, use_listpage_resources: bool, node: str) -> set:
+def _find_dbpedia_parents(graph, use_listpage_resources: bool, node: str) -> set:
     name = graph.get_name(node)
     head_lemmas = nlp_util.get_head_lemmas(nlp_util.parse(name))
     type_lexicalisation_scores = defaultdict(lambda: 0.1, cat_axioms._get_type_surface_scores(head_lemmas))
@@ -40,7 +39,7 @@ def _find_dbpedia_parents(graph: CaLiGraph, use_listpage_resources: bool, node: 
     return dbp_store.get_independent_types(mapped_types)
 
 
-def _compute_type_resource_scores(graph: CaLiGraph, node: str, use_listpage_resources: bool) -> dict:
+def _compute_type_resource_scores(graph, node: str, use_listpage_resources: bool) -> dict:
     node_resources = {res for sn in ({node} | graph.descendants(node)) for res in graph.get_dbpedia_resources(sn, use_listpage_resources)}
     type_counts = defaultdict(int)
     for res in node_resources:
