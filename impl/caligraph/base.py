4a5664863571bc80ce3967db1178038d8d26be51
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-09-23 11:30:35 +0200

    TASK: Make sure to have no unconnected nodes after cycle removal

--- impl/caligraph/base.py
+++ impl/caligraph/base.py
@@ -5,7 +5,7 @@ import util
 def get_base_graph() -> CaLiGraph:
     global __BASE_GRAPH__
     if '__BASE_GRAPH__' not in globals():
-        initializer = lambda: CaLiGraph.build_graph().resolve_cycles()
+        initializer = lambda: CaLiGraph.build_graph().resolve_cycles().append_unconnected()
         __BASE_GRAPH__ = util.load_or_create_cache('caligraph_base', initializer)
     return __BASE_GRAPH__
 
@@ -13,7 +13,7 @@ def get_base_graph() -> CaLiGraph:
 def get_merged_ontology_graph() -> CaLiGraph:
     global __MERGED_ONTOLOGY_GRAPH__
     if '__MERGED_ONTOLOGY_GRAPH__' not in globals():
-        initializer = lambda: get_base_graph().copy().merge_ontology(False).resolve_cycles()
+        initializer = lambda: get_base_graph().copy().merge_ontology(False).resolve_cycles().append_unconnected()
         __MERGED_ONTOLOGY_GRAPH__ = util.load_or_create_cache('caligraph_merged_ontology', initializer)
     return __MERGED_ONTOLOGY_GRAPH__
 
@@ -21,6 +21,6 @@ def get_merged_ontology_graph() -> CaLiGraph:
 def get_filtered_graph() -> CaLiGraph:
     global __FILTERED_GRAPH__
     if '__FILTERED_GRAPH__' not in globals():
-        initializer = lambda: get_base_graph().copy().merge_ontology(True).resolve_cycles()
+        initializer = lambda: get_base_graph().copy().merge_ontology(True).resolve_cycles().append_unconnected()
         __FILTERED_GRAPH__ = util.load_or_create_cache('caligraph_filtered', initializer)
     return __FILTERED_GRAPH__
