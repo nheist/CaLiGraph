82b97134a05eac03a83a4230d92db70dd872c18e
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-10-02 16:48:34 +0200

    TASK: Stricter encoding for caligraph iris

--- impl/caligraph/serialize.py
+++ impl/caligraph/serialize.py
@@ -7,6 +7,7 @@ import bz2
 from typing import Optional
 import os
 import types
+import urllib.parse
 
 
 def serialize_graph(graph):
@@ -22,7 +23,7 @@ def serialize_graph(graph):
 
     # define classes
     for node in graph.traverse_topdown():
-        name = node[len(cali_util.NAMESPACE_CLG_ONTOLOGY):]
+        name = _get_item_name(node, cali_util.NAMESPACE_CLG_ONTOLOGY)
         label = graph.get_label(node)
         parents = graph.parents(node) or {Thing.iri}
         equivalents = {t for t in graph.get_parts(node) if dbp_util.is_dbp_type(t)}
@@ -33,14 +34,14 @@ def serialize_graph(graph):
 
     # define properties
     for prop in graph.get_all_properties():
-        name = prop[len(cali_util.NAMESPACE_CLG_ONTOLOGY):]
+        name = _get_item_name(prop, cali_util.NAMESPACE_CLG_ONTOLOGY)
         dbp_property = dbp_util.NAMESPACE_DBP_ONTOLOGY + name
         _add_property(caligraph_onto, name, dbp_property)
 
     # define resources
     axiom_resources = {ax[1] for n in graph.nodes for ax in graph.get_axioms(n, transitive=False)}
     for res in graph.get_all_resources() | axiom_resources:
-        name = res[len(cali_util.NAMESPACE_CLG_RESOURCE):]
+        name = _get_item_name(res, cali_util.NAMESPACE_CLG_RESOURCE)
         classes = graph.get_nodes_for_resource(res) or {Thing.iri}
         equivalent = _get_dbpedia_resource(cali_util.clg_resource2dbp_resource(res), dbpedia_resource_ns) if cali_util.clg_resource2dbp_resource(res) in dbp_store.get_resources() else None
         provenance = {_get_dbpedia_resource(prov, dbpedia_resource_ns) for prov in graph.get_resource_provenance(res)}
@@ -61,7 +62,7 @@ def serialize_graph(graph):
     metadata_lines = _get_metadata(graph)
 
     # write metadata and graph data
-    with bz2.open(result_filepath, mode='w') as f:
+    with bz2.open(result_filepath, mode='wt') as f:
         f.writelines(metadata_lines)
         with open(tmp_filepath, mode='r') as tmp:
             for line in tmp:
@@ -103,7 +104,8 @@ def _add_restriction(cls_iri: str, prop_iri: str, val: str):
 def _get_dbpedia_resource(resource_iri: str, resource_ns):
     resource = IRIS[resource_iri]
     if not resource:
-        resource = Thing(resource_iri[len(dbp_util.NAMESPACE_DBP_RESOURCE):], namespace=resource_ns)
+        name = _get_item_name(resource_iri, dbp_util.NAMESPACE_DBP_RESOURCE)
+        resource = Thing(name, namespace=resource_ns)
     return resource
 
 
@@ -115,30 +117,37 @@ def _get_metadata(graph) -> list:
     property_count = len(graph.get_all_properties())
 
     metadata = [
-        f'{void_entity} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdfs.org/ns/void#Dataset> .',
-        f'{void_entity} <http://purl.org/dc/elements/1.1/title> "CaLiGraph" .',
-        f'{void_entity} <http://www.w3.org/2000/01/rdf-schema#label> "CaLiGraph" .',
-        f'{void_entity} <http://purl.org/dc/elements/1.1/description> "{description}" .',
-        f'{void_entity} <http://purl.org/dc/terms/license> <http://www.gnu.org/copyleft/fdl.html> .',
-        f'{void_entity} <http://purl.org/dc/terms/license> <http://creativecommons.org/licenses/by-sa/3.0/> .',
-        f'{void_entity} <http://purl.org/dc/terms/creator> "Nicolas Heist" .',
-        f'{void_entity} <http://purl.org/dc/terms/creator> "Nicolas Heist" .',
-        f'{void_entity} <http://purl.org/dc/terms/creator> "Heiko Paulheim" .',
-        f'{void_entity} <http://purl.org/dc/terms/created> "2019-10-01"^^<http://www.w3.org/2001/XMLSchema#date> .',
-        f'{void_entity} <http://purl.org/dc/terms/publisher> "Nicolas Heist" .',
-        f'{void_entity} <http://purl.org/dc/terms/publisher> "Heiko Paulheim" .',
-        f'{void_entity} <http://rdfs.org/ns/void#uriSpace> "{cali_util.NAMESPACE_CLG_RESOURCE}" .',
-        f'{void_entity} <http://rdfs.org/ns/void#entities> "{entity_count}"^^<http://www.w3.org/2001/XMLSchema#integer> .',
-        f'{void_entity} <http://rdfs.org/ns/void#classes> "{class_count}"^^<http://www.w3.org/2001/XMLSchema#integer> .',
-        f'{void_entity} <http://rdfs.org/ns/void#properties> "{property_count}"^^<http://www.w3.org/2001/XMLSchema#integer> .',
-        f'{void_entity} <http://purl.org/dc/terms/source> <http://dbpedia.org/resource/DBpedia> .',
-        f'{void_entity} <http://purl.org/dc/terms/source> <http://dbpedia.org/resource/Wikipedia> .',
-        f'{void_entity} <http://xmlns.com/foaf/0.1/homepage> <http://caligraph.org> .',
-        f'{void_entity} <http://rdfs.org/ns/void#sparqlEndpoint> <http://caligraph.org/sparql> .',
+        f'{void_entity} <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://rdfs.org/ns/void#Dataset> .\n',
+        f'{void_entity} <http://purl.org/dc/elements/1.1/title> "CaLiGraph" .\n',
+        f'{void_entity} <http://www.w3.org/2000/01/rdf-schema#label> "CaLiGraph" .\n',
+        f'{void_entity} <http://purl.org/dc/elements/1.1/description> "{description}" .\n',
+        f'{void_entity} <http://purl.org/dc/terms/license> <http://www.gnu.org/copyleft/fdl.html> .\n',
+        f'{void_entity} <http://purl.org/dc/terms/license> <http://creativecommons.org/licenses/by-sa/3.0/> .\n',
+        f'{void_entity} <http://purl.org/dc/terms/creator> "Nicolas Heist" .\n',
+        f'{void_entity} <http://purl.org/dc/terms/creator> "Nicolas Heist" .\n',
+        f'{void_entity} <http://purl.org/dc/terms/creator> "Heiko Paulheim" .\n',
+        f'{void_entity} <http://purl.org/dc/terms/created> "2019-10-01"^^<http://www.w3.org/2001/XMLSchema#date> .\n',
+        f'{void_entity} <http://purl.org/dc/terms/publisher> "Nicolas Heist" .\n',
+        f'{void_entity} <http://purl.org/dc/terms/publisher> "Heiko Paulheim" .\n',
+        f'{void_entity} <http://rdfs.org/ns/void#uriSpace> "{cali_util.NAMESPACE_CLG_RESOURCE}" .\n',
+        f'{void_entity} <http://rdfs.org/ns/void#entities> "{entity_count}"^^<http://www.w3.org/2001/XMLSchema#integer> .\n',
+        f'{void_entity} <http://rdfs.org/ns/void#classes> "{class_count}"^^<http://www.w3.org/2001/XMLSchema#integer> .\n',
+        f'{void_entity} <http://rdfs.org/ns/void#properties> "{property_count}"^^<http://www.w3.org/2001/XMLSchema#integer> .\n',
+        f'{void_entity} <http://purl.org/dc/terms/source> <http://dbpedia.org/resource/DBpedia> .\n',
+        f'{void_entity} <http://purl.org/dc/terms/source> <http://dbpedia.org/resource/Wikipedia> .\n',
+        f'{void_entity} <http://xmlns.com/foaf/0.1/homepage> <http://caligraph.org> .\n',
+        f'{void_entity} <http://rdfs.org/ns/void#sparqlEndpoint> <http://caligraph.org/sparql> .\n',
     ]
 
     for node in graph.nodes:
         for part in graph.get_parts(node):
-            metadata.append(f'<{node}> <http://www.w3.org/ns/prov#wasDerivedFrom> <{part}> .')
+            metadata.append(f'<{node}> <http://www.w3.org/ns/prov#wasDerivedFrom> <{part}> .\n')
 
     return metadata
+
+
+def _get_item_name(item: str, namespace: str) -> str:
+    name = item[len(namespace):]
+    for c in ['\'', '"', '´', '`']:
+        name = name.replace(c, urllib.parse.quote_plus(c))
+    return name
