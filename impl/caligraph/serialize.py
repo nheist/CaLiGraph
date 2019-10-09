f426e43ded3c02d2e2dceead36a592bc9398b855
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-10-09 14:40:49 +0200

    TASK: More finegrained release files

--- impl/caligraph/serialize.py
+++ impl/caligraph/serialize.py
@@ -3,87 +3,36 @@ import impl.dbpedia.store as dbp_store
 import impl.dbpedia.util as dbp_util
 import impl.caligraph.util as cali_util
 import bz2
-from typing import Optional
 import impl.util.serialize as serialize_util
 import impl.util.rdf as rdf_util
 import datetime
 
 
 def serialize_graph(graph):
-    lines = []
+    _write_lines_to_file(_get_lines_metadata(graph), 'results.caligraph.metadata')
+    _write_lines_to_file(_get_lines_ontology(graph), 'results.caligraph.ontology')
+    _write_lines_to_file(_get_lines_ontology_dbpedia_mapping(graph), 'results.caligraph.ontology_dbpedia-mapping')
+    _write_lines_to_file(_get_lines_ontology_provenance(graph), 'results.caligraph.ontology_provenance')
+    _write_lines_to_file(_get_lines_instances_types(graph), 'results.caligraph.instances_types')
+    _write_lines_to_file(_get_lines_instances_transitive_types(graph), 'results.caligraph.instances_transitive-types')
+    _write_lines_to_file(_get_lines_instances_labels(graph), 'results.caligraph.instances_labels')
+    _write_lines_to_file(_get_lines_instances_relations(graph), 'results.caligraph.instances_relations')
+    _write_lines_to_file(_get_lines_instances_dbpedia_mapping(graph), 'results.caligraph.instances_dbpedia-mapping')
+    _write_lines_to_file(_get_lines_instances_provenance(graph), 'results.caligraph.instances_provenance')
 
-    # void metadata
-    lines.extend(_get_metadata(graph))
 
-    # ontology metadata
-    ontology_resource = 'http://caligraph.org/ontology'
-    lines.extend([
-        serialize_util.as_object_triple(ontology_resource, rdf_util.PREDICATE_TYPE, 'http://www.w3.org/2002/07/owl#Ontology'),
-        serialize_util.as_literal_triple(ontology_resource, 'http://purl.org/dc/terms/created', _get_creation_date()),
-        serialize_util.as_literal_triple(ontology_resource, rdf_util.PREDICATE_LABEL, 'CaLiGraph Ontology'),
-        serialize_util.as_literal_triple(ontology_resource, 'http://www.w3.org/2002/07/owl#versionInfo', util.get_config('caligraph.version')),
-    ])
-
-    # classes
-    for node in graph.traverse_topdown():
-        label = graph.get_label(node)
-        parents = graph.parents(node) or {rdf_util.CLASS_OWL_THING}
-        equivalents = {t for t in graph.get_parts(node) if dbp_util.is_dbp_type(t)}
-        sources = graph.get_parts(node)
-        lines.extend(_serialize_class(node, label, parents, equivalents, sources))
-
-    # properties
-    for prop in graph.get_all_properties():
-        equivalent_property = cali_util.clg_type2dbp_type(prop)
-        lines.extend(_serialize_property(prop, equivalent_property))
-
-    # resources
-    axiom_resources = {ax[1] for n in graph.nodes for ax in graph.get_axioms(n, transitive=False) if cali_util.is_clg_resource(ax[1])}
-    for res in graph.get_all_resources() | axiom_resources:
-        label = graph.get_label(res)
-        types = graph.get_nodes_for_resource(res)
-        equivalent = cali_util.clg_resource2dbp_resource(res) if cali_util.clg_resource2dbp_resource(res) in dbp_store.get_resources() else None
-        provenance = graph.get_resource_provenance(res)
-        lines.extend(_serialize_resource(res, label, types, equivalent, provenance))
-
-    # restrictions
-    blank_node_counter = 1
-    for node in graph.nodes:
-        for prop, val in graph.get_axioms(node, transitive=False):
-            lines.extend(_serialize_restriction(node, prop, val, blank_node_counter))
-            blank_node_counter += 1
-
-    # persist minimal version
-    result_filepath = util.get_results_file('results.caligraph.minimal')
-    with bz2.open(result_filepath, mode='wt') as f:
-        f.writelines(lines)
-
-    # transitive types, materialized axioms
-    for res in graph.get_all_resources():
-        direct_types = graph.get_nodes_for_resource(res)
-        transitive_types = {tt for t in direct_types for tt in graph.ancestors(t)}.difference(direct_types)
-        lines.extend([serialize_util.as_object_triple(res, rdf_util.PREDICATE_TYPE, tt) for tt in transitive_types])
-
-        axioms = {a for t in direct_types for a in graph.get_axioms(t)}
-        for prop, val in axioms:
-            if val.startswith(cali_util.NAMESPACE_CLG_RESOURCE):
-                lines.append(serialize_util.as_object_triple(res, prop, val))
-            else:
-                lines.append(serialize_util.as_literal_triple(res, prop, val))
-
-    # persist full version
-    result_filepath = util.get_results_file('results.caligraph.materialized')
-    with bz2.open(result_filepath, mode='wt') as f:
+def _write_lines_to_file(lines: list, filepath_config: str):
+    filepath = util.get_results_file(filepath_config)
+    with bz2.open(filepath, mode='wt') as f:
         f.writelines(lines)
 
 
-def _get_metadata(graph) -> list:
+def _get_lines_metadata(graph) -> list:
     void_resource = 'http://caligraph.org/.well-known/void'
     description = 'The CaLiGraph is a large-scale general-purpose knowledge graph that extends DBpedia with a more fine-grained and restrictive ontology as well as additional resources extracted from Wikipedia Listpages.'
     entity_count = len(graph.get_all_resources())
     class_count = len(graph.nodes)
     property_count = len(graph.get_all_properties())
-
     return [
         serialize_util.as_object_triple(void_resource, rdf_util.PREDICATE_TYPE, 'http://rdfs.org/ns/void#Dataset'),
         serialize_util.as_literal_triple(void_resource, 'http://purl.org/dc/elements/1.1/title', 'CaLiGraph'),
@@ -107,37 +56,41 @@ def _get_metadata(graph) -> list:
     ]
 
 
+def _get_lines_ontology(graph) -> list:
+    lines_ontology = []
+    # metadata
+    ontology_resource = 'http://caligraph.org/ontology'
+    lines_ontology.extend([
+        serialize_util.as_object_triple(ontology_resource, rdf_util.PREDICATE_TYPE, 'http://www.w3.org/2002/07/owl#Ontology'),
+        serialize_util.as_literal_triple(ontology_resource, 'http://purl.org/dc/terms/created', _get_creation_date()),
+        serialize_util.as_literal_triple(ontology_resource, rdf_util.PREDICATE_LABEL, 'CaLiGraph Ontology'),
+        serialize_util.as_literal_triple(ontology_resource, 'http://www.w3.org/2002/07/owl#versionInfo', util.get_config('caligraph.version')),
+    ])
+    # classes
+    for node in graph.traverse_topdown():
+        lines_ontology.append(serialize_util.as_object_triple(node, rdf_util.PREDICATE_TYPE, rdf_util.CLASS_OWL_CLASS))
+        label = graph.get_label(node)
+        if label:
+            lines_ontology.append(serialize_util.as_literal_triple(node, rdf_util.PREDICATE_LABEL, label))
+        parents = graph.parents(node) or {rdf_util.CLASS_OWL_THING}
+        lines_ontology.extend([serialize_util.as_object_triple(node, rdf_util.PREDICATE_SUBCLASS_OF, p) for p in parents])
+    # properties
+    for prop in graph.get_all_properties():
+        lines_ontology.extend([
+            serialize_util.as_object_triple(prop, rdf_util.PREDICATE_TYPE, rdf_util.CLASS_OWL_CLASS),
+            serialize_util.as_object_triple(prop, rdf_util.PREDICATE_SUBPROPERTY_OF, rdf_util.CLASS_PROPERTY),
+        ])
+    # restrictions
+    blank_node_counter = 1
+    for node in graph.nodes:
+        for prop, val in graph.get_axioms(node, transitive=False):
+            lines_ontology.extend(_serialize_restriction(node, prop, val, blank_node_counter))
+            blank_node_counter += 1
+    return lines_ontology
+
+
 def _get_creation_date() -> datetime.datetime:
     return datetime.datetime.strptime(util.get_config('caligraph.creation_date'), '%Y-%m-%d')
-
-
-def _serialize_class(class_iri: str, label: str, parents: set, equivalents: set, sources: set) -> list:
-    result = [serialize_util.as_object_triple(class_iri, rdf_util.PREDICATE_TYPE, rdf_util.CLASS_OWL_CLASS)]
-    if label:
-        result.append(serialize_util.as_literal_triple(class_iri, rdf_util.PREDICATE_LABEL, label))
-    result.extend([serialize_util.as_object_triple(class_iri, rdf_util.PREDICATE_SUBCLASS_OF, p) for p in parents])
-    result.extend([serialize_util.as_object_triple(class_iri, rdf_util.PREDICATE_EQUIVALENT_CLASS, e) for e in equivalents])
-    result.extend([serialize_util.as_object_triple(class_iri, 'http://www.w3.org/ns/prov#wasDerivedFrom', s) for s in sources])
-    return result
-
-
-def _serialize_property(prop_iri: str, equivalent_property_iri: str) -> list:
-    return [
-        serialize_util.as_object_triple(prop_iri, rdf_util.PREDICATE_TYPE, rdf_util.CLASS_OWL_CLASS),
-        serialize_util.as_object_triple(prop_iri, rdf_util.PREDICATE_SUBPROPERTY_OF, rdf_util.CLASS_PROPERTY),
-        serialize_util.as_object_triple(prop_iri, rdf_util.PREDICATE_EQUIVALENT_PROPERTY, equivalent_property_iri),
-    ]
-
-
-def _serialize_resource(resource_iri: str, label: str, types: set, equivalent: Optional[str], provenance_iris: set) -> list:
-    result = [serialize_util.as_object_triple(resource_iri, rdf_util.PREDICATE_TYPE, rdf_util.CLASS_OWL_NAMED_INDIVIDUAL)]
-    if label:
-        result.append(serialize_util.as_literal_triple(resource_iri, rdf_util.PREDICATE_LABEL, label))
-    result.extend([serialize_util.as_object_triple(resource_iri, rdf_util.PREDICATE_TYPE, t) for t in types])
-    result.extend([serialize_util.as_object_triple(resource_iri, 'http://www.w3.org/ns/prov#wasDerivedFrom', p) for p in provenance_iris])
-    if equivalent:
-        result.append(serialize_util.as_object_triple(resource_iri, rdf_util.PREDICATE_SAME_AS, equivalent))
-    return result
 
 
 def _serialize_restriction(class_iri: str, prop_iri: str, val: str, blank_node_index: int) -> list:
@@ -152,3 +105,82 @@ def _serialize_restriction(class_iri: str, prop_iri: str, val: str, blank_node_i
     else:
         result.append(serialize_util.as_literal_triple(blank_node_iri, 'http://www.w3.org/2002/07/owl#hasValue', val))
     return result
+
+
+def _get_lines_ontology_dbpedia_mapping(graph) -> list:
+    lines_ontology_dbpedia_mapping = []
+    for node in graph.traverse_topdown():
+        equivalents = {t for t in graph.get_parts(node) if dbp_util.is_dbp_type(t)}
+        lines_ontology_dbpedia_mapping.extend([serialize_util.as_object_triple(node, rdf_util.PREDICATE_EQUIVALENT_CLASS, e) for e in equivalents])
+    for prop in graph.get_all_properties():
+        eq_prop = cali_util.clg_type2dbp_type(prop)
+        lines_ontology_dbpedia_mapping.append(serialize_util.as_object_triple(prop, rdf_util.PREDICATE_EQUIVALENT_PROPERTY, eq_prop))
+    return lines_ontology_dbpedia_mapping
+
+
+def _get_lines_ontology_provenance(graph) -> list:
+    lines_ontology_provenance = []
+    for node in graph.traverse_topdown():
+        sources = graph.get_parts(node)
+        lines_ontology_provenance.extend([serialize_util.as_object_triple(node, rdf_util.PREDICATE_WAS_DERIVED_FROM, s) for s in sources])
+    return lines_ontology_provenance
+
+
+def _get_lines_instances_types(graph) -> list:
+    lines_instances_types = []
+    axiom_resources = {ax[1] for n in graph.nodes for ax in graph.get_axioms(n, transitive=False) if cali_util.is_clg_resource(ax[1])}
+    for res in graph.get_all_resources() | axiom_resources:
+        lines_instances_types.append(serialize_util.as_object_triple(res, rdf_util.PREDICATE_TYPE, rdf_util.CLASS_OWL_NAMED_INDIVIDUAL))
+        types = graph.get_nodes_for_resource(res).difference({rdf_util.CLASS_OWL_THING})
+        lines_instances_types.extend([serialize_util.as_object_triple(res, rdf_util.PREDICATE_TYPE, t) for t in types])
+    return lines_instances_types
+
+
+def _get_lines_instances_transitive_types(graph) -> list:
+    lines_instances_transitive_types = []
+    for res in graph.get_all_resources():
+        direct_types = graph.get_nodes_for_resource(res)
+        transitive_types = {tt for t in direct_types for tt in graph.ancestors(t)}.difference(direct_types | {rdf_util.CLASS_OWL_THING})
+        lines_instances_transitive_types.extend([serialize_util.as_object_triple(res, rdf_util.PREDICATE_TYPE, tt) for tt in transitive_types])
+    return lines_instances_transitive_types
+
+
+def _get_lines_instances_labels(graph) -> list:
+    lines_instances_labels = []
+    axiom_resources = {ax[1] for n in graph.nodes for ax in graph.get_axioms(n, transitive=False) if cali_util.is_clg_resource(ax[1])}
+    for res in graph.get_all_resources() | axiom_resources:
+        label = graph.get_label(res)
+        if label:
+            lines_instances_labels.append(serialize_util.as_literal_triple(res, rdf_util.PREDICATE_LABEL, label))
+    return lines_instances_labels
+
+
+def _get_lines_instances_relations(graph) -> list:
+    lines_instances_relations = []
+    instance_relations = set()
+    for node in graph.nodes:
+        for prop, val in graph.get_axioms(node):
+            instance_relations.update({(res, prop, val) for res in graph.get_resources(node)})
+    for s, p, o in instance_relations:
+        if cali_util.is_clg_resource(o):
+            lines_instances_relations.append(serialize_util.as_object_triple(s, p, o))
+        else:
+            lines_instances_relations.append(serialize_util.as_literal_triple(s, p, o))
+    return lines_instances_relations
+
+
+def _get_lines_instances_dbpedia_mapping(graph) -> list:
+    lines_instances_dbpedia_mapping = []
+    axiom_resources = {ax[1] for n in graph.nodes for ax in graph.get_axioms(n, transitive=False) if cali_util.is_clg_resource(ax[1])}
+    for res in graph.get_all_resources() | axiom_resources:
+        equivalent_res = cali_util.clg_resource2dbp_resource(res)
+        if equivalent_res in dbp_store.get_resources():
+            lines_instances_dbpedia_mapping.append(serialize_util.as_object_triple(res, rdf_util.PREDICATE_SAME_AS, equivalent_res))
+    return lines_instances_dbpedia_mapping
+
+
+def _get_lines_instances_provenance(graph) -> list:
+    lines_instances_provenance = []
+    for res in graph.get_all_resources():
+        lines_instances_provenance.extend([serialize_util.as_object_triple(res, rdf_util.PREDICATE_WAS_DERIVED_FROM, p) for p in graph.get_resource_provenance(res)])
+    return lines_instances_provenance
