418633585fef48fe32cfafb927b1f811cde8b791
Nicolas Heist <nico@informatik.uni-mannheim.de>
2019-12-11 15:50:57 +0100

    TASK: Caligraph v12 (reuse patterns from cat2ax)

--- impl/caligraph/cali2ax.py
+++ impl/caligraph/cali2ax.py
@@ -6,15 +6,27 @@ import impl.dbpedia.store as dbp_store
 import impl.dbpedia.util as dbp_util
 import impl.category.category_set as cat_set
 import impl.category.store as cat_store
+import impl.category.base as cat_base
 import impl.util.nlp as nlp_util
+import impl.category.cat2ax as cat_axioms
+import impl.category.util as cat_util
+import impl.list.util as list_util
 
 
 PATTERN_CONF = util.get_config('cat2ax.pattern_confidence')
 
 
-def extract_axioms(graph) -> dict:
+def extract_axioms_direct(graph) -> dict:
     candidate_sets = _extract_candidate_sets(graph)
     patterns = _extract_patterns(graph, candidate_sets)
+    return _extract_axioms(graph, PATTERN_CONF, patterns)
+
+
+def extract_axioms(graph) -> dict:
+    category_graph = cat_base.get_conceptual_category_graph()
+    candidate_sets = cat_set.get_category_sets()
+    patterns = cat_axioms._extract_patterns(category_graph, candidate_sets)
+
     return _extract_axioms(graph, PATTERN_CONF, patterns)
 
 
@@ -89,6 +101,51 @@ def _get_resource_surface_scores(text):
 
 # --- PATTERN APPLICATION ---
 
+#def _extract_axioms_direct(graph, pattern_confidence, patterns) -> dict:
+#    util.get_logger().debug('CaLi2Ax: Extracting axioms..')
+#    axioms = defaultdict(set)
+#
+#    front_pattern_dict = {}
+#    for (front_pattern, back_pattern), axiom_patterns in _get_confidence_pattern_set(patterns, True, False).items():
+#        _fill_dict(front_pattern_dict, list(front_pattern), lambda d: _fill_dict(d, list(reversed(back_pattern)), axiom_patterns))
+#
+#    back_pattern_dict = {}
+#    for (front_pattern, back_pattern), axiom_patterns in _get_confidence_pattern_set(patterns, False, True).items():
+#        _fill_dict(back_pattern_dict, list(front_pattern), lambda d: _fill_dict(d, list(reversed(back_pattern)), axiom_patterns))
+#
+#    enclosing_pattern_dict = {}
+#    for (front_pattern, back_pattern), axiom_patterns in _get_confidence_pattern_set(patterns, True, True).items():
+#        _fill_dict(enclosing_pattern_dict, list(front_pattern), lambda d: _fill_dict(d, list(reversed(back_pattern)), axiom_patterns))
+#
+#    for node in graph.nodes.difference({graph.root_node}):
+#
+#        node_doc = nlp_util.parse(graph.get_label(node))
+#        node_axioms = []
+#
+#        front_prop_axiom = _find_axioms(graph, pattern_confidence, front_pattern_dict, node, node_doc)
+#        if front_prop_axiom:
+#            node_axioms.append(front_prop_axiom)
+#
+#        back_prop_axiom = _find_axioms(graph, pattern_confidence, back_pattern_dict, node, node_doc)
+#        if back_prop_axiom:
+#            node_axioms.append(back_prop_axiom)
+#
+#        enclosing_prop_axiom = _find_axioms(graph, pattern_confidence, enclosing_pattern_dict, node, node_doc)
+#        if enclosing_prop_axiom:
+#            node_axioms.append(enclosing_prop_axiom)
+#
+#        prop_axioms_by_pred = {a[1]: {x for x in node_axioms if x[1] == a[1]} for a in node_axioms}
+#        for pred, similar_prop_axioms in prop_axioms_by_pred.items():
+#            if dbp_store.is_object_property(pred):
+#                res_labels = {a[2]: dbp_store.get_label(a[2]) for a in similar_prop_axioms}
+#                similar_prop_axioms = {a for a in similar_prop_axioms if all(res_labels[a[2]] == val or res_labels[a[2]] not in val for val in res_labels.values())}
+#            best_prop_axiom = max(similar_prop_axioms, key=operator.itemgetter(3))
+#            axioms[node].add(best_prop_axiom)
+#
+#    util.get_logger().debug(f'CaLi2Ax: Extracted {sum(len(axioms) for axioms in axioms.values())} axioms for {len(axioms)} categories.')
+#    return axioms
+
+
 def _extract_axioms(graph, pattern_confidence, patterns) -> dict:
     util.get_logger().debug('CaLi2Ax: Extracting axioms..')
     axioms = defaultdict(set)
@@ -106,28 +163,37 @@ def _extract_axioms(graph, pattern_confidence, patterns) -> dict:
         _fill_dict(enclosing_pattern_dict, list(front_pattern), lambda d: _fill_dict(d, list(reversed(back_pattern)), axiom_patterns))
 
     for node in graph.nodes.difference({graph.root_node}):
-        node_doc = nlp_util.parse(graph.get_label(node))
-        node_axioms = []
+        property_frequencies = graph.get_property_frequencies(node)
+        for part in graph.get_parts(node):
+            if cat_util.is_category(part):
+                label = cat_util.category2name(part)
+            elif list_util.is_listcategory(part) or list_util.is_listpage(part):
+                label = list_util.list2name(part)
+            else:
+                continue
 
-        front_prop_axiom = _find_axioms(graph, pattern_confidence, front_pattern_dict, node, node_doc)
-        if front_prop_axiom:
-            node_axioms.append(front_prop_axiom)
+            node_doc = nlp_util.parse(label)
+            node_axioms = []
 
-        back_prop_axiom = _find_axioms(graph, pattern_confidence, back_pattern_dict, node, node_doc)
-        if back_prop_axiom:
-            node_axioms.append(back_prop_axiom)
+            front_prop_axiom = _find_axioms(pattern_confidence, front_pattern_dict, node, node_doc, property_frequencies)
+            if front_prop_axiom:
+                node_axioms.append(front_prop_axiom)
 
-        enclosing_prop_axiom = _find_axioms(graph, pattern_confidence, enclosing_pattern_dict, node, node_doc)
-        if enclosing_prop_axiom:
-            node_axioms.append(enclosing_prop_axiom)
+            back_prop_axiom = _find_axioms(pattern_confidence, back_pattern_dict, node, node_doc, property_frequencies)
+            if back_prop_axiom:
+                node_axioms.append(back_prop_axiom)
 
-        prop_axioms_by_pred = {a[1]: {x for x in node_axioms if x[1] == a[1]} for a in node_axioms}
-        for pred, similar_prop_axioms in prop_axioms_by_pred.items():
-            if dbp_store.is_object_property(pred):
-                res_labels = {a[2]: dbp_store.get_label(a[2]) for a in similar_prop_axioms}
-                similar_prop_axioms = {a for a in similar_prop_axioms if all(res_labels[a[2]] == val or res_labels[a[2]] not in val for val in res_labels.values())}
-            best_prop_axiom = max(similar_prop_axioms, key=operator.itemgetter(3))
-            axioms[node].add(best_prop_axiom)
+            enclosing_prop_axiom = _find_axioms(pattern_confidence, enclosing_pattern_dict, node, node_doc, property_frequencies)
+            if enclosing_prop_axiom:
+                node_axioms.append(enclosing_prop_axiom)
+
+            prop_axioms_by_pred = {a[1]: {x for x in node_axioms if x[1] == a[1]} for a in node_axioms}
+            for pred, similar_prop_axioms in prop_axioms_by_pred.items():
+                if dbp_store.is_object_property(pred):
+                    res_labels = {a[2]: dbp_store.get_label(a[2]) for a in similar_prop_axioms}
+                    similar_prop_axioms = {a for a in similar_prop_axioms if all(res_labels[a[2]] == val or res_labels[a[2]] not in val for val in res_labels.values())}
+                best_prop_axiom = max(similar_prop_axioms, key=operator.itemgetter(3))
+                axioms[node].add(best_prop_axiom)
 
     util.get_logger().debug(f'CaLi2Ax: Extracted {sum(len(axioms) for axioms in axioms.values())} axioms for {len(axioms)} categories.')
     return axioms
@@ -175,11 +241,11 @@ def _detect_pattern(pattern_dict, words):
     return None, None
 
 
-def _get_axioms_for_node(graph, pattern_confidence, axiom_patterns, node, text_diff):
+def _get_axioms_for_node(pattern_confidence, axiom_patterns, node, text_diff, property_frequencies):
     prop_axiom = None
 
     possible_values = _get_resource_surface_scores(text_diff)
-    props_scores = {(p, v): freq * axiom_patterns[p] * possible_values[v] for (p, v), freq in graph.get_property_frequencies(node).items() if p in axiom_patterns and v in possible_values}
+    props_scores = {(p, v): freq * axiom_patterns[p] * possible_values[v] for (p, v), freq in property_frequencies.items() if p in axiom_patterns and v in possible_values}
     prop, max_prop_score = max(props_scores.items(), key=operator.itemgetter(1), default=((None, None), 0))
     if max_prop_score >= pattern_confidence:
         pred, val = prop
@@ -188,12 +254,12 @@ def _get_axioms_for_node(graph, pattern_confidence, axiom_patterns, node, text_d
     return prop_axiom
 
 
-def _find_axioms(graph, pattern_confidence, pattern_dict, node, node_doc):
+def _find_axioms(pattern_confidence, pattern_dict, node, node_doc, property_frequencies):
     node_words = [w.text for w in node_doc]
     axiom_patterns, pattern_lengths = _detect_pattern(pattern_dict, node_words)
     if axiom_patterns:
         front_pattern_idx = pattern_lengths[0] or None
         back_pattern_idx = -1 * pattern_lengths[1] or None
         text_diff = node_doc[front_pattern_idx:back_pattern_idx].text
-        return _get_axioms_for_node(graph, pattern_confidence, axiom_patterns, node, text_diff)
+        return _get_axioms_for_node(pattern_confidence, axiom_patterns, node, text_diff, property_frequencies)
     return None
