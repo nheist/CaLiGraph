4430525944e8449e2a12986226e920a95cf21569
Nicolas Heist <nico@informatik.uni-mannheim.de>
2020-07-02 11:51:35 +0200

    TASK: Use max_cpus for parsing

--- impl/dbpedia/pages.py
+++ impl/dbpedia/pages.py
@@ -15,7 +15,7 @@ def get_all_parsed_pages() -> dict:
 
 
 def _parse_pages() -> dict:
-    with mp.Pool(processes=5) as pool:  # todo: set chunks to max_cpus if working
+    with mp.Pool(processes=util.get_config('max_cpus')) as pool:
         results = pool.map(_parse_page_chunk, _chunk_dict(get_all_pages_markup(), 10000))
     return {res: parsed_page for chunk_result in results for res, parsed_page in chunk_result.items()}
 
