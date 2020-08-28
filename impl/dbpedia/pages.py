7cc368561127314ea98841df8504d2e4d642e21f
Nicolas Heist <nico@informatik.uni-mannheim.de>
2020-08-28 08:39:45 +0200

    TASK: Add total to progress bar

--- impl/dbpedia/pages.py
+++ impl/dbpedia/pages.py
@@ -16,7 +16,7 @@ def get_all_parsed_pages() -> dict:
 
 def _parse_pages() -> dict:
     with mp.Pool(processes=util.get_config('max_cpus')) as pool:
-        results = tqdm(pool.imap(_parse_page, get_all_pages_markup().items(), chunksize=2000), desc='Parsing pages')
+        results = tqdm(pool.imap(_parse_page, get_all_pages_markup().items(), chunksize=2000), desc='Parsing pages', total=len(get_all_pages_markup()))
     return {resource: parsed_page for resource, parsed_page in results if parsed_page}
 
 
