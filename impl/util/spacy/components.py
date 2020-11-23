227ebabecdf2471e97ef40c83a86f62fdc87a32e
Nicolas Heist <nico@informatik.uni-mannheim.de>
2020-11-23 09:05:27 +0100

    FIX: appending list to tuple correctly

--- impl/util/spacy/components.py
+++ impl/util/spacy/components.py
@@ -98,5 +98,5 @@ def tag_by_phrase(doc: Doc) -> Doc:
             continue
         if doc[by_index - 1].text == '(':  # include possible parenthesis before by phrase
             by_index -= 1
-        doc.ents = doc.ents + [Span(doc, by_index, end_index, label=doc.vocab.strings['BY'])]
+        doc.ents = [*doc.ents, Span(doc, by_index, end_index, label=doc.vocab.strings['BY'])]
     return doc
