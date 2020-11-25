cf328445ae52e42e6869dc180a4cc2f5421786b3
Nicolas Heist <nico@informatik.uni-mannheim.de>
2020-11-25 13:49:51 +0100

    FIX: run xgboost only with one process to avoid internal memory errors of xgboost

--- impl/list/extract.py
+++ impl/list/extract.py
@@ -18,7 +18,7 @@ def extract_enum_entities(df: pd.DataFrame) -> dict:
     """Return entities extracted from enumeration list pages with XG-Boost-based simple/collective extraction."""
     util.get_logger().info(f'LIST/EXTRACT: Extracting entities from enumeration list pages..')
 
-    base_estimator = XGBClassifier(predictor='cpu_predictor', colsample_bytree=.8, scale_pos_weight=.25)
+    base_estimator = XGBClassifier(predictor='cpu_predictor', n_jobs=1, colsample_bytree=.8, scale_pos_weight=.25)
     config = {
         'base_estimator': base_estimator,
         'sampling_functions': [_sample_by_entity_position],
@@ -36,7 +36,7 @@ def extract_table_entities(df: pd.DataFrame) -> dict:
     util.get_logger().info(f'LIST/EXTRACT: Extracting entities from table list pages..')
     base_estimator = Pipeline([
         ('feature_selection', SelectKBest(k=100)),
-        ('classification', XGBClassifier(predictor='cpu_predictor', colsample_bytree=.8, scale_pos_weight=.25)),
+        ('classification', XGBClassifier(predictor='cpu_predictor', n_jobs=1, colsample_bytree=.8, scale_pos_weight=.25)),
     ])
     config = {
         'base_estimator': base_estimator,
