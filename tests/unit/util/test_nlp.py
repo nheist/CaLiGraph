9f187a1501cde93314539cdc0b0aec16eaf14a5d
Nicolas Heist <nico@informatik.uni-mannheim.de>
2020-12-08 10:00:39 +0100

    TASK: Add some tests for plural lexhead subject

--- tests/unit/util/test_nlp.py
+++ tests/unit/util/test_nlp.py
@@ -28,8 +28,22 @@ def _without_by_phrase(with_by_phrase: str, without_by_phrase: str):
     assert with_removed_by_phrase.lower() == without_by_phrase.lower(), f'{with_by_phrase} should be converted to {without_by_phrase}'
 
 
-def test_get_head_lemmas():
-    pass
+def test_has_plural_lexhead_subjects():
+    _text_has_plural_lexhead_subjects('Reptiles')
+    _text_has_plural_lexhead_subjects('Princesses')
+    _text_has_plural_lexhead_subjects('African musical instruments')
+
+    _text_has_no_plural_lexhead_subjects('London')
+
+
+@check_func
+def _text_has_plural_lexhead_subjects(text: str):
+    assert nlp_util.has_plural_lexhead_subjects(text)
+
+
+@check_func
+def _text_has_no_plural_lexhead_subjects(text: str):
+    assert not nlp_util.has_plural_lexhead_subjects(text)
 
 
 def test_singularize_phrase():
