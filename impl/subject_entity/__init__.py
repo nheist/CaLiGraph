612fc5890110ce183009b312e8fa92465a007fd6
Nicolas Heist <nico@informatik.uni-mannheim.de>
2021-02-15 07:48:31 +0100

    FIX: Add rounding statement for processes

--- impl/subject_entity/__init__.py
+++ impl/subject_entity/__init__.py
@@ -35,7 +35,7 @@ def _retrieve_training_data(graph) -> tuple:
     list_nlp._initialise_parser()  # make sure that parser has been trained before going into multiprocessing
 
     train_tokens, train_labels = [], []
-    with mp.Pool(processes=utils.get_config('max_cpus') / 4) as pool:
+    with mp.Pool(processes=round(utils.get_config('max_cpus') / 4)) as pool:
         ctx = [(lp_uri, lp_data, graph) for lp_uri, lp_data in list_store.get_parsed_listpages().items()]
         for token_lists, label_lists in tqdm(pool.imap_unordered(tokenize.page_to_tokens_and_labels, ctx, chunksize=200), desc='Extracting BERT training data', total=len(ctx)):
             train_tokens.extend(token_lists)
