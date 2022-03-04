fac83df5cc5da22e7f193d39f2b54af52fe56fe7
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-03-04 20:21:46 +0100

    REFACTOR: pos labels with begin und inside

--- impl/subject_entity/preprocess/pos_label.py
+++ impl/subject_entity/preprocess/pos_label.py
@@ -6,18 +6,24 @@ import impl.dbpedia.store as dbp_store
 class POSLabel(Enum):
     NONE = 0
     PERSON = 1
-    NORP = 2
-    FAC = 3
-    ORG = 4
-    GPE = 5
-    LOC = 6
-    PRODUCT = 7
-    EVENT = 8
-    WORK_OF_ART = 9
-    LAW = 10
-    LANGUAGE = 11
-    SPECIES = 12
-    OTHER = 13
+    NORP = 3
+    FAC = 5
+    ORG = 7
+    GPE = 9
+    LOC = 11
+    PRODUCT = 13
+    EVENT = 15
+    WORK_OF_ART = 17
+    LAW = 19
+    LANGUAGE = 21
+    SPECIES = 23
+    OTHER = 25
+
+    def begin(self):
+        return self.value
+
+    def inside(self):
+        return self.value + 1 if self.value > 0 else self.value
 
 
 def map_entities_to_pos_labels(entity_chunks: list) -> list:
@@ -29,7 +35,14 @@ def _map_entity_chunk(entity_chunk: list) -> list:
     # first find the pos labels for all entities in the chunk (to avoid duplicate resolution of pos labels)
     entity_to_pos_label_mapping = {ent: _find_pos_label_for_ent(ent) for ent in set(entity_chunk)}
     # then map the entities to pos labels
-    return [entity_to_pos_label_mapping[ent] for ent in entity_chunk]
+    pos_labels = []
+    for idx, ent in enumerate(entity_chunk):
+        pos_label = entity_to_pos_label_mapping[ent]
+        if idx == 0 or ent != entity_chunk[idx - 1]:
+            pos_labels.append(pos_label.begin())
+        else:
+            pos_labels.append(pos_label.inside())
+    return pos_labels
 
 
 def _find_pos_label_for_ent(ent: str) -> POSLabel:
@@ -40,45 +53,45 @@ def _find_pos_label_for_ent(ent: str) -> POSLabel:
     for t in dbp_store.get_types(ent_uri):
         if t in ttl_mapping:
             return ttl_mapping[t]
-    return POSLabel.OTHER.value
+    return POSLabel.OTHER
 
 
 def _get_type_to_label_mapping() -> dict:
     return {
         # PERSON
-        'http://dbpedia.org/ontology/Person': POSLabel.PERSON.value,
-        'http://dbpedia.org/ontology/Deity': POSLabel.PERSON.value,
+        'http://dbpedia.org/ontology/Person': POSLabel.PERSON,
+        'http://dbpedia.org/ontology/Deity': POSLabel.PERSON,
         # NORP
-        'http://dbpedia.org/ontology/PoliticalParty': POSLabel.NORP.value,
-        'http://dbpedia.org/ontology/Family': POSLabel.NORP.value,
-        'http://dbpedia.org/ontology/EthnicGroup': POSLabel.NORP.value,
+        'http://dbpedia.org/ontology/PoliticalParty': POSLabel.NORP,
+        'http://dbpedia.org/ontology/Family': POSLabel.NORP,
+        'http://dbpedia.org/ontology/EthnicGroup': POSLabel.NORP,
         # FAC
-        'http://dbpedia.org/ontology/ArchitecturalStructure': POSLabel.FAC.value,
-        'http://dbpedia.org/ontology/Mine': POSLabel.FAC.value,
-        'http://dbpedia.org/ontology/Monument': POSLabel.FAC.value,
+        'http://dbpedia.org/ontology/ArchitecturalStructure': POSLabel.FAC,
+        'http://dbpedia.org/ontology/Mine': POSLabel.FAC,
+        'http://dbpedia.org/ontology/Monument': POSLabel.FAC,
         # ORG
-        'http://dbpedia.org/ontology/Organisation': POSLabel.ORG.value,
+        'http://dbpedia.org/ontology/Organisation': POSLabel.ORG,
         # GPE
-        'http://dbpedia.org/ontology/PopulatedPlace': POSLabel.GPE.value,
+        'http://dbpedia.org/ontology/PopulatedPlace': POSLabel.GPE,
         # LOC
-        'http://dbpedia.org/ontology/Place': POSLabel.LOC.value,
-        'http://dbpedia.org/ontology/Location': POSLabel.LOC.value,
+        'http://dbpedia.org/ontology/Place': POSLabel.LOC,
+        'http://dbpedia.org/ontology/Location': POSLabel.LOC,
         # PRODUCT
-        'http://dbpedia.org/ontology/Food': POSLabel.PRODUCT.value,
-        'http://dbpedia.org/ontology/MeanOfTransportation': POSLabel.PRODUCT.value,
-        'http://dbpedia.org/ontology/Software': POSLabel.PRODUCT.value,
-        'http://dbpedia.org/ontology/Device': POSLabel.PRODUCT.value,
+        'http://dbpedia.org/ontology/Food': POSLabel.PRODUCT,
+        'http://dbpedia.org/ontology/MeanOfTransportation': POSLabel.PRODUCT,
+        'http://dbpedia.org/ontology/Software': POSLabel.PRODUCT,
+        'http://dbpedia.org/ontology/Device': POSLabel.PRODUCT,
         # EVENT
-        'http://dbpedia.org/ontology/Event': POSLabel.EVENT.value,
+        'http://dbpedia.org/ontology/Event': POSLabel.EVENT,
         # WORK_OF_ART
-        'http://dbpedia.org/ontology/Work': POSLabel.WORK_OF_ART.value,
-        'http://dbpedia.org/ontology/Award': POSLabel.WORK_OF_ART.value,
+        'http://dbpedia.org/ontology/Work': POSLabel.WORK_OF_ART,
+        'http://dbpedia.org/ontology/Award': POSLabel.WORK_OF_ART,
         # LAW
-        'http://dbpedia.org/ontology/Law': POSLabel.LAW.value,
-        'http://dbpedia.org/ontology/LegalCase': POSLabel.LAW.value,
-        'http://dbpedia.org/ontology/Treaty': POSLabel.LAW.value,
+        'http://dbpedia.org/ontology/Law': POSLabel.LAW,
+        'http://dbpedia.org/ontology/LegalCase': POSLabel.LAW,
+        'http://dbpedia.org/ontology/Treaty': POSLabel.LAW,
         # LANGUAGE
-        'http://dbpedia.org/ontology/Language': POSLabel.LANGUAGE.value,
+        'http://dbpedia.org/ontology/Language': POSLabel.LANGUAGE,
         # SPECIES
-        'http://dbpedia.org/ontology/Species': POSLabel.SPECIES.value
+        'http://dbpedia.org/ontology/Species': POSLabel.SPECIES
     }
