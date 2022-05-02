a7603ade0225224100d83411142e9aff3b168d01
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-05-02 14:20:59 +0200

    REFACTOR: consistent devices

--- entity_linking/preprocessing/embeddings.py
+++ entity_linking/preprocessing/embeddings.py
@@ -4,14 +4,13 @@ from impl.dbpedia.resource import DbpResourceStore
 
 
 class EntityIndexToEmbeddingMapper(nn.Module):
-    def __init__(self, ent_dim: int):
+    def __init__(self, ent_dim: int, device):
         super().__init__()
         # create a tensor containing entity embeddings (use entity idx as position; "empty" positions are all zeros)
         embedding_vecs = DbpResourceStore.instance().get_embedding_vectors()
-        self.entity_embeddings = torch.zeros((max(embedding_vecs) + 1, ent_dim))
+        self.entity_embeddings = torch.zeros((max(embedding_vecs) + 1, ent_dim), device=device)
         for e_idx, vec in embedding_vecs.items():
-            self.entity_embeddings[e_idx] = torch.Tensor(vec)
+            self.entity_embeddings[e_idx] = torch.tensor(vec, device=device)
 
     def forward(self, entity_indices: torch.Tensor) -> torch.Tensor:
-        out_device = entity_indices.device
-        return self.entity_embeddings[entity_indices.to('cpu')].to(out_device)
+        return self.entity_embeddings[entity_indices]
