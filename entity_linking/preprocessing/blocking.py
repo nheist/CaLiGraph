9f13096c31e7d9e1dd2638ccb3e7379247e1151a
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-06-02 08:32:26 +0200

    FIX: Move stopword generation to init in blocker

--- entity_linking/preprocessing/blocking.py
+++ entity_linking/preprocessing/blocking.py
@@ -13,6 +13,7 @@ class WordBlocker:
         for res_idx in dbr.get_embedding_vectors():
             for sf in dbr.get_resource_by_idx(res_idx).get_surface_forms():
                 self.entity_blocks[self._block_surface_form(sf.split())].add(res_idx)
+        self.stopwords = set(stopwords.words('english'))
 
     def get_entity_indices_for_words(self, words: List[str]) -> Set[int]:
         return self.entity_blocks[self._block_surface_form(words)]
@@ -20,7 +21,7 @@ class WordBlocker:
     def _block_surface_form(self, sf_words: List[str]) -> Tuple[str]:
         sf_words = {unidecode(w.lower()) for w in sf_words}  # lowercase everything and convert special chars
         sf_words = {self._make_alphanum(w) for w in sf_words if w}  # remove non-alphanumeric characters
-        filtered_sf_words = sf_words.difference(stopwords.words('english'))
+        filtered_sf_words = sf_words.difference(self.stopwords)
         return tuple(sorted(filtered_sf_words or sf_words))
 
     @classmethod
