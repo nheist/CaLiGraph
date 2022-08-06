05388fc1830ab9ace486ba2c9cbca08ba78e9ca7
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-08-06 07:54:02 +0200

    TASK: Implement negative sampling mechanism

--- impl/subject_entity/preprocess/sample.py
+++ impl/subject_entity/preprocess/sample.py
@@ -1,4 +1,7 @@
 from typing import Tuple, List
+from collections import defaultdict
+import random
+from copy import deepcopy
 import numpy as np
 import torch
 from torch.utils.data import Dataset
@@ -6,7 +9,7 @@ import utils
 from impl import wikipedia
 from impl.dbpedia.resource import DbpListpage
 from impl.util.transformer import EntityIndex
-from .word_tokenize import WordTokenizer, WordTokenizedPage
+from .word_tokenize import WordTokenizer, WordTokenizedPage, ListingType, WordTokenizedListing
 from .pos_label import POSLabel, map_entities_to_pos_labels
 from .heuristics import find_subject_entities_for_listpage
 
@@ -62,8 +65,52 @@ def _chunk_word_tokenized_pages(page_data: List[WordTokenizedPage], negative_sam
         ws_chunks.extend(page_ws_chunks)
         label_chunks.extend(page_label_chunks)
     if negative_sample_size > 0:
-        pass  # TODO: implement negative sampling mechanism
+        # compute number of negative chunks to create
+        num_enum_chunks = len([c for c in context_chunks if c['listing_type'] == ListingType.ENUMERATION])
+        num_table_chunks = len(context_chunks) - num_enum_chunks
+        num_negative_enum_chunks = int(num_enum_chunks * negative_sample_size)
+        num_negative_table_chunks = int(num_table_chunks * negative_sample_size)
+        # collect all contexts and items
+        enum_listings = [l for p in page_data for l in p.listings if l.listing_type == ListingType.ENUMERATION]
+        enum_contexts = {0: [(l.listing_type, l.topsection, l.section, l.context) for l in enum_listings]}
+        enum_items = {0: [i for l in enum_listings for i in l.items]}
+        table_listings = [l for p in page_data for l in p.listings if l.listing_type == ListingType.TABLE]
+        table_contexts = defaultdict(list)
+        table_items = defaultdict(list)
+        for l in table_listings:
+            table_contexts[l.column_count].append((l.listing_type, l.topsection, l.section, l.context))
+            table_items[l.column_count].extend(l.items)
+        # generate negatives
+        for _ in range(num_negative_enum_chunks):
+            n_ctx, n_tokens, n_ws, n_labels = _generate_negative_tokenized_page_chunk(enum_contexts, enum_items, max_items_per_chunk, max_words_per_chunk)
+            context_chunks.append(n_ctx)
+            token_chunks.append(n_tokens)
+            ws_chunks.append(n_ws)
+            label_chunks.append(n_labels)
+        for _ in range(num_negative_table_chunks):
+            n_ctx, n_tokens, n_ws, n_labels = _generate_negative_tokenized_page_chunk(table_contexts, table_items, max_items_per_chunk, max_words_per_chunk)
+            context_chunks.append(n_ctx)
+            token_chunks.append(n_tokens)
+            ws_chunks.append(n_ws)
+            label_chunks.append(n_labels)
     return context_chunks, token_chunks, ws_chunks, label_chunks
+
+
+def _generate_negative_tokenized_page_chunk(contexts: dict, items: dict, max_items_per_chunk: int, max_words_per_chunk: int) -> Tuple[list, list, list, list]:
+    # pick listing quantifier randomly but weighted by occurrence (for enums we have only one quantifier, making this step irrelevant)
+    quantifier = random.choices(list(items), weights=[len(vals) for vals in items.values()], k=1)[0]
+    # pick number of items to put in the chunk
+    num_items = random.choice(range(3, max_items_per_chunk + 1))
+    # draw context and items
+    listing_type, listing_topsection, listing_section, listing_context = contexts[quantifier]
+    listing_items = [deepcopy(i) for i in random.choices(items[quantifier], k=num_items)]
+    for i in listing_items:  # set all labels to NO_ENTITY as we are generating negatives
+        i.entity_indices = [EntityIndex.NO_ENTITY] * len(i.entity_indices)
+    # manufacture page and convert to chunks
+    listing = WordTokenizedListing(listing_type, listing_context, listing_items, listing_topsection, listing_section)
+    page = WordTokenizedPage(-1, [listing])
+    context_chunks, token_chunks, ws_chunks, label_chunks = page.to_chunks(max_items_per_chunk, max_words_per_chunk)
+    return context_chunks[0], token_chunks[0], ws_chunks[0], label_chunks[0]
 
 
 def _encode_labels(labels: List[List[str]], encodings) -> List[List[str]]:
