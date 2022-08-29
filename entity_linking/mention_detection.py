0370529a7271d9df288acec8d2c7648f71deb2e3
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-08-29 12:02:30 +0200

    FIX: Fix incorrect assignment of enum/table listings in test predictions

--- entity_linking/mention_detection.py
+++ entity_linking/mention_detection.py
@@ -11,7 +11,7 @@ from entity_linking.evaluation.mention_detection import SETagsEvaluator
 
 
 def run_evaluation(model_name: str, epochs: int, batch_size: int, learning_rate: float, warmup_steps: int, weight_decay: float, ignore_tags: bool, predict_single_tag: bool, negative_sample_size: float, single_item_chunks: bool, train_on_listpages: bool, train_on_pages: bool, save_as: str):
-    run_id = f'v3_{model_name}_tlp-{train_on_pages}_tp-{train_on_pages}_it-{ignore_tags}_st-{predict_single_tag}_nss-{negative_sample_size}_sic-{single_item_chunks}_e-{epochs}_lr-{learning_rate}_ws-{warmup_steps}_wd-{weight_decay}'
+    run_id = f'v4_{model_name}_tlp-{train_on_pages}_tp-{train_on_pages}_it-{ignore_tags}_st-{predict_single_tag}_nss-{negative_sample_size}_sic-{single_item_chunks}_e-{epochs}_lr-{learning_rate}_ws-{warmup_steps}_wd-{weight_decay}'
     # check whether to use a local model or a predefined one from huggingface
     local_path_to_model = f'./entity_linking/MD/models/{model_name}'
     model_name = local_path_to_model if os.path.isdir(local_path_to_model) else model_name
@@ -23,11 +23,7 @@ def run_evaluation(model_name: str, epochs: int, batch_size: int, learning_rate:
     else:
         model = AutoModelForTokenClassification.from_pretrained(model_name, num_labels=number_of_labels)
         model.resize_token_embeddings(len(tokenizer))
-    # load test data
-    test_data = utils.load_or_create_cache('MD_page_test_data', prepare.get_md_page_test_data)
-    test_dataset = prepare_dataset(test_data, tokenizer, ignore_tags, predict_single_tag, single_item_chunks)
 
-    trainer = None
     if train_on_listpages:
         # load data
         train_data, val_data = utils.load_or_create_cache('MD_listpage_data', prepare.get_md_listpage_data)
@@ -43,7 +39,7 @@ def run_evaluation(model_name: str, epochs: int, batch_size: int, learning_rate:
             logging_dir=f'./entity_linking/MD/logs/LP_{run_id}',
             logging_steps=500,
             evaluation_strategy=IntervalStrategy.STEPS,
-            eval_steps=5000,
+            eval_steps=3000,
             per_device_train_batch_size=batch_size,
             per_device_eval_batch_size=batch_size,
             num_train_epochs=epochs,
@@ -83,6 +79,10 @@ def run_evaluation(model_name: str, epochs: int, batch_size: int, learning_rate:
         trainer = Trainer(model=model, args=training_args, train_dataset=p_train_dataset)
         trainer.train()
 
+    # make test predictions
+    test_data = utils.load_or_create_cache('MD_page_test_data', prepare.get_md_page_test_data)
+    test_dataset = prepare_dataset(test_data, tokenizer, ignore_tags, predict_single_tag, single_item_chunks)
+    trainer = Trainer(model=model, compute_metrics=lambda eval_prediction: SETagsEvaluator(eval_prediction, test_dataset.listing_types, predict_single_tag).evaluate())
     test_metrics = trainer.evaluate(test_dataset, metric_key_prefix='test')
     tb = SummaryWriter(log_dir=f'./entity_linking/MD/logs/{run_id}')
     for key, val in test_metrics.items():
