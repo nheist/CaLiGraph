5cb0c6bcab7aa1a44deff639cad027c5d558c0fd
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-09-20 07:26:00 +0200

    TASK: increase chunk size for chunking

--- impl/subject_entity/mention_detection/data/chunking.py
+++ impl/subject_entity/mention_detection/data/chunking.py
@@ -81,7 +81,7 @@ def _chunk_listings(listings: List[WikiListing], labels: Optional[Dict[int, Dict
     else:
         listings_with_labels = [(listing, labels[listing.page.idx][listing.idx]) for listing in listings if listing.page.idx in labels and listing.idx in labels[listing.page.idx]]
     with mp.Pool(processes=4) as pool:
-        yield from pool.imap_unordered(_chunk_listing, tqdm(listings_with_labels, desc='Chunking listings'), chunksize=1000)
+        yield from pool.imap_unordered(_chunk_listing, tqdm(listings_with_labels, desc='Chunking listings'), chunksize=10000)
 
 
 def _chunk_listing(listing_and_labels: Tuple[WikiListing, Optional[Dict[int, List[Union[int, List[int]]]]]]) -> Tuple[WikiListing, List[List[str]], List[List[str]], List[List[int]], List[List[int]]]:
