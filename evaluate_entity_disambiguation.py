c3e89689c2029399b1b2fc3578efd987c0af27b6
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-10-04 10:44:33 +0200

    REFACTOR: Restructure eval script for ED

--- evaluate_entity_disambiguation.py
+++ evaluate_entity_disambiguation.py
@@ -1,19 +1,19 @@
 import argparse
 import os
-from entity_linking.entity_disambiguation.matching import MatchingScenario, MatchingApproach
+os.environ['DISABLE_SPACY_CACHE'] = '1'
 
 
 if __name__ == '__main__':
-    # first parse all the arguments
+    # make relevant imports and parse all the arguments
+    from entity_linking.entity_disambiguation.matching import MatchingScenario, MatchingApproach
     parser = argparse.ArgumentParser(description='Run the evaluation of entity disambiguation approaches.')
     parser.add_argument('gpu', type=int, choices=range(8), help='Number of GPU to use')
     parser.add_argument('scenario', type=str, choices=[s.value for s in MatchingScenario], help='Whether to match mention-mention or mention-entity')
     parser.add_argument('approach', type=str, choices=[a.value for a in MatchingApproach], help='Approach used for matching')
     parser.add_argument('-sa', '--save_alignment', action=argparse.BooleanOptionalAction, default=False, help='Whether to save the produced alignment for train/val/test')
     args = parser.parse_args()
-    # then set necessary environment variables
+    # and set necessary environment variables
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
-    os.environ['DISABLE_SPACY_CACHE'] = '1'
     # then fix all seeds
     import random
     import numpy as np
