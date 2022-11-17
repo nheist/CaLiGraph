48017bd5bc9b25185dbdbebff4d07e73fd04f258
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-11-17 20:51:02 +0100

    FIX: Fix param dict

--- entity_linking/matching/matcher.py
+++ entity_linking/matching/matcher.py
@@ -19,9 +19,9 @@ class Matcher(ABC):
 
     def get_approach_name(self) -> str:
         param_dict = self._get_param_dict()
-        if 'base_model' in param_dict and param_dict['base_model'].startswith('sentence-transformers/'):
-            param_dict['base_model'] = param_dict['base_model'][len('sentence-transformers/'):]
-        approach_params = [self.id] + [f'{k}={v}' for k, v in self._get_param_dict().items()]
+        if 'bm' in param_dict and param_dict['bm'].startswith('sentence-transformers/'):
+            param_dict['bm'] = param_dict['bm'][len('sentence-transformers/'):]
+        approach_params = [self.id] + [f'{k}={v}' for k, v in param_dict.items()]
         return '_'.join(approach_params)
 
     def _get_param_dict(self) -> dict:
