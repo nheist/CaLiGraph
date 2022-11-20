c54ef67a201aadd039336ba9854c97992c4047e5
Nicolas Heist <nico@informatik.uni-mannheim.de>
2022-11-20 13:15:05 +0100

    REFACTOR: Simplify clustering

--- entity_linking/matching/greedy_clustering.py
+++ entity_linking/matching/greedy_clustering.py
@@ -1,4 +1,4 @@
-from typing import Set, List, Iterable
+from typing import Set, List, Iterable, Tuple, Optional
 from collections import defaultdict
 import itertools
 import networkx as nx
@@ -25,20 +25,7 @@ class GreedyClusteringMatcher(MatcherWithCandidates):
     def predict(self, eval_mode: str, data_corpus: DataCorpus) -> CandidateAlignment:
         ag = self._get_alignment_graph(eval_mode, True)
         valid_subgraphs = self._compute_valid_subgraphs(ag)
-        ca = CandidateAlignment()
-        clustering = []
-        for g in valid_subgraphs:
-            mention_nodes = self._get_mention_nodes(g)
-            clustering.append(mention_nodes)
-            for mention_pair in itertools.combinations(mention_nodes, 2):
-                ca.add_candidate(mention_pair, 1)
-            ent_nodes = self._get_entity_nodes(g)
-            if ent_nodes:
-                ent_node = ent_nodes.pop()
-                for m_id in mention_nodes:
-                    ca.add_candidate((m_id, ent_node), 1)
-        ca.add_entity_clustering(clustering)
-        return ca
+        return self._create_alignment([(self._get_mention_nodes(g), self._get_entity_node(g)) for g in valid_subgraphs])
 
     def _get_alignment_graph(self, eval_mode: str, add_entities: bool) -> nx.Graph:
         utils.get_logger().debug('Initializing alignment graph..')
@@ -48,13 +35,24 @@ class GreedyClusteringMatcher(MatcherWithCandidates):
                 if score <= self.me_threshold:
                     continue
                 ag.add_node(e_id, is_ent=True)
-                ag.add_edge(e_id, m_id, weight=min(0, 1-score))
-        ag.add_weighted_edges_from([(u, v, min(0, 1-score)) for (u, v), score in self.mm_ca[eval_mode].get_mm_candidates(True) if score > self.mm_threshold])
+                ag.add_edge(e_id, m_id, weight=max(0, 1-score))
+        ag.add_weighted_edges_from([(u, v, max(0, 1-score)) for (u, v), score in self.mm_ca[eval_mode].get_mm_candidates(True) if score > self.mm_threshold])
         return ag
 
     def _get_subgraphs(self, ag: nx.Graph) -> Iterable[nx.Graph]:
         for nodes in nx.connected_components(ag):
             yield ag.subgraph(nodes)
+
+    def _create_alignment(self, clusters: List[Tuple[Set[MentionId], Optional[int]]]) -> CandidateAlignment:
+        ca = CandidateAlignment()
+        for mentions, ent_id in clusters:
+            for mention_pair in itertools.combinations(mentions, 2):
+                ca.add_candidate(mention_pair, 1)
+            if ent_id is not None:
+                for m_id in mentions:
+                    ca.add_candidate((m_id, ent_id), 1)
+        ca.add_entity_clustering([mentions for mentions, _ in clusters])
+        return ca
 
     def _compute_valid_subgraphs(self, ag: nx.Graph) -> List[nx.Graph]:
         utils.get_logger().debug('Computing valid subgraphs..')
@@ -68,6 +66,14 @@ class GreedyClusteringMatcher(MatcherWithCandidates):
 
     def _is_valid_graph(self, ag: nx.Graph) -> bool:
         return len(self._get_entity_nodes(ag)) <= 1
+
+    @classmethod
+    def _get_entity_node(cls, g: nx.Graph) -> Optional[int]:
+        for node, is_ent in g.nodes(data='is_ent'):
+            if not is_ent:
+                continue
+            return node
+        return None
 
     @classmethod
     def _get_entity_nodes(cls, g: nx.Graph) -> Set[int]:
